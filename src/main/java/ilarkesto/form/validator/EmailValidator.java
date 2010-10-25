8770e579c8a0f7a47210e687c91475eb9727913c
Witoslaw Koczewski <wi@koczewski.de>
2010-10-25 02:02:00 +0200

    email address parsing

--- src/main/java/ilarkesto/form/validator/EmailValidator.java
+++ src/main/java/ilarkesto/form/validator/EmailValidator.java
@@ -1,9 +1,7 @@
 package ilarkesto.form.validator;
 
+import ilarkesto.email.EmailAddress;
 import ilarkesto.form.ValidationException;
-
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
 
 /**
  * @author <A href="mailto:wi@koczewski.de">Witoslaw Koczewski </A>
@@ -11,29 +9,25 @@ import javax.mail.internet.InternetAddress;
  */
 public class EmailValidator implements Validator {
 
-    public static final EmailValidator THIS = new EmailValidator();
+	public static final EmailValidator THIS = new EmailValidator();
 
-    private static final String MSG = "Die Eingabe muss eine g\u00FCltige Email-Adresse sein. Beispiel: \"wi@koczewski.de\" oder \"Witoslaw Koczewski <wi@koczewski.de>\"";
+	private static final String MSG = "Die Eingabe muss eine g\u00FCltige Email-Adresse sein. Beispiel: \"wi@koczewski.de\" oder \"Witoslaw Koczewski <wi@koczewski.de>\"";
 
-    // overriding
-    public String validate(String s) throws ValidationException {
-        s = s.trim();
-        if (s.length() < 5) throw new ValidationException(MSG);
-        int idx = s.indexOf('@');
-        if (idx < 1) throw new ValidationException(MSG);
+	@Override
+	public String validate(String s) throws ValidationException {
+		s = s.trim();
+		if (s.length() < 5) throw new ValidationException(MSG);
+		int idx = s.indexOf('@');
+		if (idx < 1) throw new ValidationException(MSG);
 
-        // if (idx >= s.length() - 3) throw new ValidationException(MSG);
-        // int idx2 = s.lastIndexOf('.');
-        // if (idx2 < 3) throw new ValidationException(MSG);
-        // if (idx2 <= idx) throw new ValidationException(MSG);
-        // if (idx2 >= s.length() - 1) throw new ValidationException(MSG);
+		// if (idx >= s.length() - 3) throw new ValidationException(MSG);
+		// int idx2 = s.lastIndexOf('.');
+		// if (idx2 < 3) throw new ValidationException(MSG);
+		// if (idx2 <= idx) throw new ValidationException(MSG);
+		// if (idx2 >= s.length() - 1) throw new ValidationException(MSG);
 
-        try {
-            InternetAddress.parse(s.replace(';', ','));
-        } catch (AddressException ex) {
-            throw new ValidationException(ex.getLocalizedMessage() + ". " + MSG);
-        }
-        return s;
-    }
+		EmailAddress.parseList(s);
+		return s;
+	}
 
 }
