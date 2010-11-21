6bb811beadd2ca5d54088759b89e65cba14a5eea
Witoslaw Koczewski <wi@koczewski.de>
2010-11-22 01:10:00 +0200

    output dir for tests

--- src/main/java/ilarkesto/testng/ATest.java
+++ src/main/java/ilarkesto/testng/ATest.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 
 public class ATest extends Assert {
 
+	public static final String OUTPUT_DIR = "test-output";
+
 	public static void assertStartsWith(String actual, String expectedPrefix) {
 		assertTrue(actual.startsWith(expectedPrefix), "<" + actual + "> expected to start with <" + expectedPrefix
 				+ "> |");
