00cb9df0f3ced0c47d90af387f5309ba7c84833b
Witoslaw Koczewski <wi@koczewski.de>
2011-03-17 01:02:00 +0200

    gwt: scroll to top on page switch

--- src/main/java/ilarkesto/gwt/client/SwitcherWidget.java
+++ src/main/java/ilarkesto/gwt/client/SwitcherWidget.java
@@ -1,28 +1,27 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
  * 
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
- * License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
- * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
- * for more details.
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
  * 
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
 package ilarkesto.gwt.client;
 
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.ScrollPanel;
 import com.google.gwt.user.client.ui.Widget;
 
 public class SwitcherWidget extends AWidget {
 
 	private Widget currentWidget;
 	private boolean height100;
-	private ScrollPanel scrollPanel;
 
 	public SwitcherWidget(boolean height100) {
 		this.height100 = height100;
@@ -35,14 +34,11 @@ public class SwitcherWidget extends AWidget {
 	}
 
 	public <W extends Widget> W show(W widget) {
-		if (currentWidget == widget) {
-			// update();
-			return widget;
-		}
+		if (currentWidget == widget) return widget;
 		currentWidget = widget;
 		replaceContent(currentWidget);
 		update();
-		if (scrollPanel != null) scrollPanel.scrollToTop();
+		Window.scrollTo(0, 0);
 		return widget;
 	}
 
@@ -52,10 +48,6 @@ public class SwitcherWidget extends AWidget {
 
 	public Widget getCurrentWidget() {
 		return currentWidget;
-	}
-
-	public void setScrollPanel(ScrollPanel scrollPanel) {
-		this.scrollPanel = scrollPanel;
 	}
 
 	@Override
