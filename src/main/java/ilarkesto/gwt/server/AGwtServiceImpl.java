a95d96a38c849902154702fd141c22175cb1e6e2
Witoslaw Koczewski <wi@koczewski.de>
2011-03-28 00:06:00 +0200

    gwt: workaround for SecurityException

--- src/main/java/ilarkesto/gwt/server/AGwtServiceImpl.java
+++ src/main/java/ilarkesto/gwt/server/AGwtServiceImpl.java
@@ -1,13 +1,13 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
  * 
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
- * License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
- * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
- * for more details.
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
  * 
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
@@ -74,6 +74,13 @@ public abstract class AGwtServiceImpl extends RemoteServiceServlet {
 		AWebSession session = getSession();
 		if (session.isSessionInvalidated())
 			getWebApplication().destroyWebSession(session, getThreadLocalRequest().getSession());
+	}
+
+	@Override
+	protected void checkPermutationStrongName() throws SecurityException {
+		// TODO remove this to use super.checkPermutationStrongName();
+		// Workaround for SecurityException: Blocked request without GWT permutation header (XSRF attack?)
+		// @see http://code.google.com/p/gwteventservice/issues/detail?id=30
 	}
 
 	protected final AWebSession getSession() {
