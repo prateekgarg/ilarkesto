7f65cb2833e045369c0e8d14b7132f9bfee1f761
Witoslaw Koczewski <wi@koczewski.de>
2011-03-31 23:44:36 +0200

    tasks

--- src/main/java/ilarkesto/concurrent/ATask.java
+++ src/main/java/ilarkesto/concurrent/ATask.java
@@ -1,13 +1,13 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
  * 
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
- * License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
- * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
- * for more details.
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
  * 
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
@@ -145,6 +145,20 @@ public abstract class ATask {
 	@Override
 	public String toString() {
 		return getClass().getSimpleName();
+	}
+
+	public Runnable createRunnable() {
+		return new Runnable() {
+
+			@Override
+			public void run() {
+				ATask.this.run();
+			}
+		};
+	}
+
+	public Thread createThread() {
+		return new Thread(createRunnable());
 	}
 
 }
