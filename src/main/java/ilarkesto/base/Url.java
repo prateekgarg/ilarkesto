402c86f0055c6d51fae0c17715817efb5d323fbf
Witoslaw Koczewski <wi@koczewski.de>
2011-04-07 18:27:20 +0200

    html renderer

--- src/main/java/ilarkesto/base/Url.java
+++ src/main/java/ilarkesto/base/Url.java
@@ -1,13 +1,13 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
  * 
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
- * License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
- * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
- * for more details.
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
  * 
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
@@ -24,69 +24,76 @@ import java.util.Map;
  */
 public final class Url implements BeanStorage<String> {
 
-    private String base;
-    private Map<String, String> parameters;
-    private boolean immutable;
+	private String base;
+	private Map<String, String> parameters;
+	private boolean immutable;
 
-    public Url(Url template) {
-        this.base = template.base;
-        this.parameters = template.parameters == null ? null : new HashMap<String, String>(template.parameters);
-    }
+	public Url(Url template) {
+		this.base = template.base;
+		this.parameters = template.parameters == null ? null : new HashMap<String, String>(template.parameters);
+	}
 
-    public Url(String base) {
-        this.base = base;
-    }
+	public Url(String base) {
+		this.base = base;
+	}
 
-    public boolean isInternal() {
-        return !isExternal();
-    }
+	public boolean isInternal() {
+		return !isExternal();
+	}
 
-    public boolean isExternal() {
-        return isExternal(base);
-    }
+	public boolean isExternal() {
+		return isExternal(base);
+	}
 
-    public static boolean isExternal(String url) {
-        return url.startsWith("http://") || url.startsWith("ftp://") || url.startsWith("mailto:")
-                || url.startsWith("javascript:");
-    }
+	public static boolean isExternal(String url) {
+		return url.startsWith("http://") || url.startsWith("ftp://") || url.startsWith("mailto:")
+				|| url.startsWith("javascript:");
+	}
 
-    public String getBase() {
-        return base;
-    }
+	public String getBase() {
+		return base;
+	}
 
-    public Url putAll(Map<String, ? extends String> parameters) {
-        if (immutable) throw new RuntimeException("Url is immutable");
-        if (this.parameters == null) this.parameters = new HashMap<String, String>();
-        this.parameters.putAll(parameters);
-        return this;
-    }
+	@Override
+	public Url putAll(Map<String, ? extends String> parameters) {
+		if (immutable) throw new RuntimeException("Url is immutable");
+		if (this.parameters == null) this.parameters = new HashMap<String, String>();
+		this.parameters.putAll(parameters);
+		return this;
+	}
 
-    public Url put(String name, String value) {
-        if (immutable) throw new RuntimeException("Url is immutable");
-        if (value == null) return this;
-        if (parameters == null) parameters = new HashMap<String, String>();
-        parameters.put(name, value);
-        return this;
-    }
+	@Override
+	public Url put(String name, String value) {
+		if (immutable) throw new RuntimeException("Url is immutable");
+		if (value == null) return this;
+		if (parameters == null) parameters = new HashMap<String, String>();
+		parameters.put(name, value);
+		return this;
+	}
 
-    public Url put(String name, Integer value) {
-        if (value == null) return this;
-        return put(name, value.toString());
-    }
+	public Url put(String name, Boolean value) {
+		if (value == null) return this;
+		return put(name, value.toString());
+	}
 
-    public Url put(String name, Long value) {
-        if (value == null) return this;
-        return put(name, value.toString());
-    }
+	public Url put(String name, Integer value) {
+		if (value == null) return this;
+		return put(name, value.toString());
+	}
 
-    public Url setImmutable() {
-        this.immutable = true;
-        return this;
-    }
+	public Url put(String name, Long value) {
+		if (value == null) return this;
+		return put(name, value.toString());
+	}
 
-    @Override
-    public String toString() {
-        return Str.constructUrl(base, parameters == null ? null : parameters);
-    }
+	public Url setImmutable() {
+		this.immutable = true;
+		return this;
+	}
+
+	@Override
+	public String toString() {
+		return Str.constructUrl(base, parameters == null ? null : parameters);
+	}
 
 }
--- src/main/java/ilarkesto/ui/web/HtmlRenderer.java
+++ src/main/java/ilarkesto/ui/web/HtmlRenderer.java
@@ -1,13 +1,13 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
  * 
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
- * License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
- * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
- * for more details.
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
  * 
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
@@ -488,6 +488,10 @@ public class HtmlRenderer {
 
 	public void SCRIPTdojo() {
 		SCRIPTjavascript("http://ajax.googleapis.com/ajax/libs/dojo/1.5/dojo/dojo.xd.js", null);
+	}
+
+	public void SCRIPTjquery() {
+		SCRIPTjavascript("http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js", null);
 	}
 
 	public Tag startBODY() {
