bd8a6c3217128964005ae8a8bdab924b352f3570
Witoslaw Koczewski <wi@koczewski.de>
2011-04-28 00:38:02 +0200

    email

--- src/main/java/ilarkesto/email/Eml.java
+++ src/main/java/ilarkesto/email/Eml.java
@@ -1,13 +1,13 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
  * 
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
- * License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
- * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
- * for more details.
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
  * 
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
@@ -299,7 +299,7 @@ public class Eml {
 		boolean destinationOpened = false;
 		Folder source = message.getFolder();
 		try {
-			if (!source.isOpen()) {
+			if (source != null && !source.isOpen()) {
 				source.open(Folder.READ_ONLY);
 				sourceOpened = true;
 			}
@@ -307,10 +307,14 @@ public class Eml {
 				destination.open(Folder.READ_WRITE);
 				destinationOpened = true;
 			}
-			try {
-				source.copyMessages(new Message[] { message }, destination);
-			} catch (MessagingException e) {
+			if (source == null) {
 				destination.appendMessages(new Message[] { message });
+			} else {
+				try {
+					source.copyMessages(new Message[] { message }, destination);
+				} catch (MessagingException e) {
+					destination.appendMessages(new Message[] { message });
+				}
 			}
 		} catch (MessagingException ex) {
 			throw new RuntimeException("Copying message " + toString(message) + " from " + source.getName() + " to "
