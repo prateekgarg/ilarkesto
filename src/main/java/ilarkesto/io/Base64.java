5fc301b0e645737ce989ab78b7ceeb389ea26de6
Witoslaw Koczewski <wi@koczewski.de>
2011-07-19 17:08:52 +0200

    encryption

--- src/main/java/ilarkesto/io/Base64.java
+++ src/main/java/ilarkesto/io/Base64.java
@@ -1,17 +1,12 @@
 /*
- * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
+ * Copyright unknown
  * 
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
- * License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
- * 
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
- * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
- * for more details.
- * 
- * You should have received a copy of the GNU General Public License along with this program. If not, see
- * <http://www.gnu.org/licenses/>.
+ * I am placing this code in the Public Domain. Do with it as you will. This software comes with no guarantees
+ * or warranties but with plenty of well-wishing instead! Please visit <a
+ * href="http://iharder.net/base64">http://iharder.net/base64</a> periodically to check for updates or to
+ * contribute improvements.
  */
+
 package ilarkesto.io;
 
 /**
@@ -85,13 +80,6 @@ package ilarkesto.io;
  * <li>v1.3.4 - Fixed when "improperly padded stream" error was thrown at the wrong time.</li>
  * <li>v1.3.3 - Fixed I/O streams which were totally messed up.</li>
  * </ul>
- * 
- * <p>
- * I am placing this code in the Public Domain. Do with it as you will. This software comes with no guarantees
- * or warranties but with plenty of well-wishing instead! Please visit <a
- * href="http://iharder.net/base64">http://iharder.net/base64</a> periodically to check for updates or to
- * contribute improvements.
- * </p>
  * 
  * @author Robert Harder
  * @author rob@iharder.net
@@ -870,7 +858,7 @@ public class Base64 {
 		catch (java.io.UnsupportedEncodingException uee) {
 			bytes = s.getBytes();
 		} // end catch
-		// </change>
+			// </change>
 
 		// Decode
 		bytes = decode(bytes, 0, bytes.length, options);
