7e86583f96d67104a0ac1ee2b61837bc6b35b127
Witoslaw Koczewski <wi@koczewski.de>
2011-08-23 10:21:35 +0200

    bugfix

--- src/main/java/ilarkesto/form/MultiItemFormField.java
+++ src/main/java/ilarkesto/form/MultiItemFormField.java
@@ -1,13 +1,13 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
  * 
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
- * License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
- * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
- * for more details.
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
  * 
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
@@ -45,6 +45,8 @@ public class MultiItemFormField<T> extends AFormField {
 
 	private StringProvider<T> itemLabelProvider;
 
+	private StringProvider<T> itemImageUrlProvider;
+
 	private String noSelectionLabel;
 
 	public MultiItemFormField(String name) {
@@ -71,6 +73,14 @@ public class MultiItemFormField<T> extends AFormField {
 
 	public StringProvider<T> getItemLabelProvider() {
 		return itemLabelProvider;
+	}
+
+	public void setItemImageUrlProvider(StringProvider<T> imageUrlProvider) {
+		this.itemImageUrlProvider = imageUrlProvider;
+	}
+
+	public StringProvider<T> getItemImageUrlProvider() {
+		return itemImageUrlProvider;
 	}
 
 	public MultiItemFormField<T> setItemTooltipProvider(StringProvider<T> itemTooltipProvider) {
@@ -155,14 +165,17 @@ public class MultiItemFormField<T> extends AFormField {
 		return value;
 	}
 
+	@Override
 	public String getValueAsString() {
 		return value == null ? "0" : String.valueOf(value.size());
 	}
 
+	@Override
 	public void update(Map<String, String> data, Collection<FileItem> uploadedFiles) {
-	// nop
+		// nop
 	}
 
+	@Override
 	public void validate() throws ValidationException {
 		if (isRequired() && (value == null || value.size() == 0))
 			throw new ValidationException("Hier ist eine Auswahl erforderlich.");
--- src/main/java/ilarkesto/ui/action/MultiOptionAction.java
+++ src/main/java/ilarkesto/ui/action/MultiOptionAction.java
@@ -1,13 +1,13 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
  * 
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
- * License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
- * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
- * for more details.
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
  * 
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
@@ -95,7 +95,7 @@ public final class MultiOptionAction<T> extends AAction {
 		if (payloadIdGenerator == null) payloadIdGenerator = new CountingIdGenerator("p");
 		for (T o : payloads) {
 			String icon = o instanceof Iconized ? ((Iconized) o).getIcon() : "item";
-			addOption(new Option<T>(payloadIdGenerator.generateId(), o.toString(), icon, o));
+			addOption(new Option<T>(payloadIdGenerator.generateId(), o.toString(), icon, null, o));
 		}
 	}
 
