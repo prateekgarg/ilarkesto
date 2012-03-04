3113d5b0d6de084486c95392c604f313d82c15de
Witoslaw Koczewski <wi@koczewski.de>
2012-03-05 00:01:36 +0100

    reflection

--- src/main/java/ilarkesto/base/Reflect.java
+++ src/main/java/ilarkesto/base/Reflect.java
@@ -1,13 +1,13 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
  * 
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
- * License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
- * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
- * for more details.
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
  * 
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
@@ -220,6 +220,11 @@ public abstract class Reflect {
 			throw new NullPointerException("Method does not exist: " + object.getClass() + "." + method + "("
 					+ Str.concat(getClassSimpleNames(parameters), ", ") + ")");
 		return invoke(object, m, parameters);
+	}
+
+	public static boolean invokeBool(Object object, String method, Object... parameters) {
+		Boolean ret = (Boolean) invoke(object, method, parameters);
+		return ret;
 	}
 
 	public static Object invoke(Object object, Method method, Object... parameters) {
