32f1e69139143186e844707b0b74c849b8ef6996
Witoslaw Koczewski <wi@koczewski.de>
2012-03-05 00:04:42 +0100

    plain text as default

--- src/main/java/ilarkesto/form/TextareaFormField.java
+++ src/main/java/ilarkesto/form/TextareaFormField.java
@@ -1,13 +1,13 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
  * 
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
- * License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
- * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
- * for more details.
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
  * 
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
@@ -15,7 +15,6 @@
 package ilarkesto.form;
 
 import ilarkesto.base.Str;
-import ilarkesto.richtext.RichTextFormatter;
 
 public class TextareaFormField extends TextFormField {
 
@@ -41,13 +40,6 @@ public class TextareaFormField extends TextFormField {
 		s = Str.cutHtmlAndHeaderAndBody(s);
 		s = s.replace("<title></title>", "");
 		return s;
-	}
-
-	@Override
-	public TextFormField setValue(String value) {
-		if (!html) return super.setValue(value);
-		if (value == null || value.startsWith("<html")) return super.setValue(value);
-		return super.setValue(RichTextFormatter.toHtml(value));
 	}
 
 	private int lines = 15;
@@ -78,7 +70,7 @@ public class TextareaFormField extends TextFormField {
 	}
 
 	public boolean isForceHtml() {
-		return true; // TODO forceHtml;
+		return false;
 	}
 
 }
