f793673650af0c600b4fe1b3e2996866dba110ff
Witoslaw Koczewski <wi@koczewski.de>
2012-07-05 00:20:50 +0200

    log more detials on LDAP error

--- src/main/java/ilarkesto/integration/ldap/Ldap.java
+++ src/main/java/ilarkesto/integration/ldap/Ldap.java
@@ -1,13 +1,13 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
  * 
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
- * License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
- * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
- * for more details.
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
  * 
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
@@ -16,12 +16,14 @@ package ilarkesto.integration.ldap;
 
 import ilarkesto.auth.AuthenticationFailedException;
 import ilarkesto.auth.WrongPasswordException;
+import ilarkesto.core.base.Str;
 import ilarkesto.core.logging.Log;
 
 import java.util.Hashtable;
 
 import javax.naming.AuthenticationException;
 import javax.naming.Context;
+import javax.naming.Name;
 import javax.naming.NamingEnumeration;
 import javax.naming.NamingException;
 import javax.naming.directory.Attribute;
@@ -90,8 +92,19 @@ public class Ldap {
 		} catch (AuthenticationException ex) {
 			throw ex;
 		} catch (NamingException ex) {
-			throw new RuntimeException(ex);
+			StringBuilder sb = new StringBuilder();
+			sb.append("Creating InitialDirContext failed.");
+			sb.append("\n    URL: ").append(url);
+			String explanation = ex.getExplanation();
+			if (!Str.isBlank(explanation)) sb.append("\n    explanation: ").append(explanation);
+			Name remainingName = ex.getRemainingName();
+			if (remainingName != null) sb.append("\n    remaining name: ").append(remainingName.toString());
+			Name resolvedName = ex.getResolvedName();
+			if (resolvedName != null) sb.append("\n    resolved name: ").append(resolvedName.toString());
+			Object resolvedObj = ex.getResolvedObj();
+			if (resolvedObj != null) sb.append("\n    resolved object: ").append(resolvedObj.toString());
+			sb.append("\n  Exception:");
+			throw new RuntimeException(sb.toString(), ex);
 		}
 	}
-
 }
