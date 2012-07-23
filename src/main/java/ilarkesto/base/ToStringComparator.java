f0157f53be09d86093ab59fde86d41031a10fe72
Witoslaw Koczewski <wi@koczewski.de>
2012-07-24 00:11:29 +0200

    toStringComparator

--- src/main/java/ilarkesto/base/ToStringComparator.java
+++ src/main/java/ilarkesto/base/ToStringComparator.java
@@ -1,13 +1,13 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
  * 
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
- * License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
- * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
- * for more details.
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
  * 
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
@@ -29,10 +29,15 @@ public class ToStringComparator implements Comparator {
 
 	@Override
 	public int compare(Object a, Object b) {
+		String as = a.toString();
+		String bs = b.toString();
+		if (as == null && bs == null) return 0;
+		if (as == null) return 1;
+		if (bs == null) return -1;
 		if (ignoreCase) {
-			return a.toString().compareToIgnoreCase(b.toString());
+			return as.compareToIgnoreCase(bs);
 		} else {
-			return a.toString().compareTo(b.toString());
+			return as.compareTo(bs);
 		}
 	}
 
