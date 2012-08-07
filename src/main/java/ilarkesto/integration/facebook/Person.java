a1f9f83b59c1ca52044262163768a64a032728f0
Witoslaw Koczewski <wi@koczewski.de>
2012-08-07 12:52:24 +0200

    facebook

--- src/main/java/ilarkesto/integration/facebook/Person.java
+++ src/main/java/ilarkesto/integration/facebook/Person.java
@@ -12,6 +12,26 @@ public class Person extends AIdentity {
 		return json.getString("name");
 	}
 
+	public String getFirstName() {
+		return json.getString("first_name");
+	}
+
+	public String getLastName() {
+		return json.getString("last_name");
+	}
+
+	public String getGender() {
+		return json.getString("gender");
+	}
+
+	public boolean isMale() {
+		return "male".equals(getGender());
+	}
+
+	public boolean isFemale() {
+		return "female".equals(getGender());
+	}
+
 	public String getUsername() {
 		return json.getString("username");
 	}
