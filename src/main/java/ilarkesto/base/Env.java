f788225e0f5a7c5f789d2cc83105a5d5e1df3f17
Witoslaw Koczewski <wi@koczewski.de>
2012-09-03 00:23:20 +0200

    file size on linux

--- src/main/java/ilarkesto/base/Env.java
+++ src/main/java/ilarkesto/base/Env.java
@@ -1,13 +1,13 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
  * 
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
- * License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
- * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
- * for more details.
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
  * 
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
@@ -287,13 +287,13 @@ public abstract class Env {
 			BufferedReader in = null;
 			Process proc = null;
 			try {
-				proc = Runtime.getRuntime().exec(new String[] { "du", "-s0", file.getAbsolutePath() });
+				proc = Runtime.getRuntime().exec(new String[] { "du", "-sd0", file.getAbsolutePath() });
 				in = new BufferedReader(new InputStreamReader(proc.getInputStream()));
 				String s = in.readLine().trim();
 				int idx = s.indexOf('\t');
 				if (idx < 1) throw new RuntimeException("Unexpected output from 'du' command: " + s);
 				s = s.substring(0, idx);
-				return Long.parseLong(s) * 1000;
+				return Long.parseLong(s);
 			} catch (IOException ex) {
 				throw new RuntimeException(ex);
 			} finally {
