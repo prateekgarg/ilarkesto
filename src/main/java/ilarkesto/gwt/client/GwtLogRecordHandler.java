913064aced24dab729c1c5699a42e7f8d1bd0e96
Witoslaw Koczewski <wi@koczewski.de>
2012-11-15 00:31:35 +0100

    gwt bugfix

--- src/main/java/ilarkesto/gwt/client/GwtLogRecordHandler.java
+++ src/main/java/ilarkesto/gwt/client/GwtLogRecordHandler.java
@@ -1,13 +1,13 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
  * 
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
- * License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
- * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
- * for more details.
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
  * 
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
@@ -23,9 +23,17 @@ public class GwtLogRecordHandler implements LogRecordHandler {
 
 	@Override
 	public void log(LogRecord record) {
-		if (GWT.isScript()) return;
-		System.out.println(record.toString());
+		if (GWT.isScript()) {
+			logToConsole(record.toString());
+		} else {
+			System.out.println(record.toString());
+		}
 	}
+
+	private static native void logToConsole(String message)
+	/*-{
+	 	if (console && console.log) console.log(message);
+	}-*/;
 
 	@Override
 	public void flush() {}
