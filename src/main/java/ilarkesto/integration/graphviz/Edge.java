4915cd4c8c5152726dbe433140ab7a72210eb85d
Witoslaw Koczewski <wi@koczewski.de>
2014-05-13 16:36:56 +0200

    graphviz

--- src/main/java/ilarkesto/integration/graphviz/Edge.java
+++ src/main/java/ilarkesto/integration/graphviz/Edge.java
@@ -1,13 +1,13 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
  * 
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
- * License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
- * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
- * for more details.
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
  * 
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
@@ -26,10 +26,19 @@ public class Edge {
 	Edge(Node from, Node to) {
 		this.from = from;
 		this.to = to;
+		fontsize(10);
 	}
 
 	public Edge label(String label) {
-		properties.put("label", "\"" + label + "\"");
+		return property("label", "\"" + label + "\"");
+	}
+
+	public Edge fontsize(int pts) {
+		return property("fontsize", String.valueOf(pts));
+	}
+
+	private Edge property(String name, String value) {
+		if (value != null) properties.put(name, value);
 		return this;
 	}
 
