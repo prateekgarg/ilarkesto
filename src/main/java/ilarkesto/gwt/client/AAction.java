4980d09e9219e9c63b2baeb41acff5e706d540fd
Witoslaw Koczewski <wi@koczewski.de>
2014-06-28 15:37:00 +0200

    GWT actions

--- src/main/java/ilarkesto/gwt/client/AAction.java
+++ src/main/java/ilarkesto/gwt/client/AAction.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
@@ -26,6 +26,8 @@ public abstract class AAction implements Command, ClickHandler {
 
 	protected final Log log = Log.get(getClass());
 	private Updatable updatable = RootWidgetUpdatable.INSTANCE;
+
+	private ClickEvent clickEvent;
 
 	public abstract String getLabel();
 
@@ -45,6 +47,7 @@ public abstract class AAction implements Command, ClickHandler {
 
 	@Override
 	public void onClick(ClickEvent event) {
+		this.clickEvent = event;
 		event.stopPropagation();
 		execute();
 	}
@@ -83,4 +86,19 @@ public abstract class AAction implements Command, ClickHandler {
 		return this;
 	}
 
+	protected Updatable getUpdatable() {
+		return updatable;
+	}
+
+	protected boolean isUpdatableSet() {
+		return updatable != null;
+	}
+
+	protected ClickEvent getClickEvent() {
+		return clickEvent;
+	}
+
+	protected boolean isClickEventSet() {
+		return clickEvent != null;
+	}
 }
