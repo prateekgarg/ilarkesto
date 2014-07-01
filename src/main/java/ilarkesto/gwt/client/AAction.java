1e404430bae40a6af0d49119d3ed4e54f3e7da0f
Witoslaw Koczewski <wi@koczewski.de>
2014-07-01 19:37:04 +0200

    gwt persistence

--- src/main/java/ilarkesto/gwt/client/AAction.java
+++ src/main/java/ilarkesto/gwt/client/AAction.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
@@ -16,6 +16,8 @@ package ilarkesto.gwt.client;
 
 import ilarkesto.core.base.Str;
 import ilarkesto.core.logging.Log;
+import ilarkesto.core.persistance.AEntityDatabase;
+import ilarkesto.core.persistance.Transaction;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
@@ -37,7 +39,17 @@ public abstract class AAction implements Command, ClickHandler {
 	public final void execute() {
 		if (!isExecutable()) throw new RuntimeException("Action not executable: " + this);
 		if (!isPermitted()) throw new RuntimeException("Action not permitted: " + this);
-		onExecute();
+		if (AEntityDatabase.instance == null) {
+			onExecute();
+		} else {
+			Transaction transaction = Transaction.get().setAutoCommit(false);
+			try {
+				onExecute();
+				transaction.commit();
+			} finally {
+				transaction.setAutoCommit(true);
+			}
+		}
 		if (updatable != null) updatable.update();
 	}
 
--- src/main/java/ilarkesto/mda/legacy/generator/EntityGenerator.java
+++ src/main/java/ilarkesto/mda/legacy/generator/EntityGenerator.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
@@ -196,7 +196,6 @@ public class EntityGenerator extends DatobGenerator<EntityModel> {
 
 					if (ref.isCollection()) {
 						if (ref.isUnique()) {
-							comment("ref == unique");
 							ln("        if (get" + Str.uppercaseFirstLetter(br.getName()) + "() != null) get"
 									+ Str.uppercaseFirstLetter(br.getName()) + "().remove"
 									+ Str.uppercaseFirstLetter(ref.getNameSingular()) + "((" + bean.getBeanClass()
@@ -208,7 +207,6 @@ public class EntityGenerator extends DatobGenerator<EntityModel> {
 									+ ")this);");
 						}
 					} else {
-						comment("ref != collection");
 						if (ref.isUnique()) {
 							ln("        if (get" + Str.uppercaseFirstLetter(br.getName()) + "() != null) get"
 									+ Str.uppercaseFirstLetter(br.getName()) + "().set"
