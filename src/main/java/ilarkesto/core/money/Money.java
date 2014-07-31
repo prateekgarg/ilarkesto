cf5e0ce23d49c2bbf354cd853a8e5444592b7322
Witoslaw Koczewski <wi@koczewski.de>
2014-07-31 16:24:38 +0200

    Bugfix: Money currency

--- src/main/java/ilarkesto/core/money/Money.java
+++ src/main/java/ilarkesto/core/money/Money.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
@@ -78,7 +78,7 @@ public final class Money implements Comparable<Money>, Serializable, Formatable 
 		if (currency.equals("$")) currency = "USD";
 		if (currency.length() != 3)
 			throw new IllegalArgumentException("Unsupported currency. Only USD, EUR, PLN,... allowed. -> " + currency);
-		currency = currency.toLowerCase();
+		currency = currency.toUpperCase();
 		return currency;
 	}
 
