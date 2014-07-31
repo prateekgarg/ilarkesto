0f06e5a0bb998d9921559d3b283613ac0b62f10c
Witoslaw Koczewski <wi@koczewski.de>
2014-07-31 14:46:20 +0200

    PDF table row keep together test

--- src/test/java/ilarkesto/integration/itext/PdfBuilderTest.java
+++ src/test/java/ilarkesto/integration/itext/PdfBuilderTest.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- *
+ * 
  * You should have received a copy of the GNU Affero General Public License along with this program. If not,
  * see <http://www.gnu.org/licenses/>.
  */
@@ -29,6 +29,23 @@ import java.io.File;
 import org.testng.annotations.Test;
 
 public class PdfBuilderTest extends ATest {
+
+	@Test
+	public void tableRowKeepTogether() {
+		PdfBuilder pdf = new PdfBuilder();
+
+		ATable table = pdf.table(1);
+
+		for (int i = 0; i < 50; i++) {
+			ARow row = table.row();
+			if (i > 30) row.setKeepTogether(true);
+			row.cell().text("row " + i);
+		}
+
+		File pdfFile = getTestOutputFile("tableRowKeepTogether.pdf");
+		log.info(pdfFile.getAbsolutePath());
+		pdf.write(pdfFile);
+	}
 
 	@Test
 	public void multiPageTable() {
