63cae3f8fd1e63dab810c763ac287fe2331ce3d3
Witoslaw Koczewski <wi@koczewski.de>
2014-08-05 14:50:11 +0200

    servlet

--- src/main/java/ilarkesto/webapp/AServlet.java
+++ src/main/java/ilarkesto/webapp/AServlet.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License along with this program. If not,
  * see <http://www.gnu.org/licenses/>.
  */
@@ -123,7 +123,7 @@ public abstract class AServlet<A extends AWebApplication, S extends AWebSession>
 		return true;
 	}
 
-	private void handleError(Throwable ex, RequestWrapper<S> req) {
+	protected void handleError(Throwable ex, RequestWrapper<S> req) {
 		log.info("request caused error:", req, ex);
 		req.sendErrorInternal(Str.format(ex));
 	}
