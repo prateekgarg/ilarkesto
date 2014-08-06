7a26e4901532ee37f92790089526321f296edbde
Witoslaw Koczewski <wi@koczewski.de>
2014-08-06 14:20:53 +0200

    MDA Generator

--- src/main/java/ilarkesto/mda/legacy/model/DatobModel.java
+++ src/main/java/ilarkesto/mda/legacy/model/DatobModel.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
@@ -92,6 +92,8 @@ public class DatobModel extends BeanModel {
 
 	public SimplePropertyModel addProperty(String name, Class type) {
 		SimplePropertyModel propertyModel = new SimplePropertyModel(this, name, false, false, type.getName());
+		if (type.equals(boolean.class) || type.equals(int.class) || type.equals(long.class) || type.equals(float.class)
+				|| type.equals(double.class)) propertyModel.setMandatory(true);
 		properties.add(propertyModel);
 		return propertyModel;
 	}
