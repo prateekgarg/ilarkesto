5bf3969d3998876f9927ed41818947e61c37f090
Witoslaw Koczewski <wi@koczewski.de>
2014-08-07 13:36:57 +0200

    Util

--- src/main/java/ilarkesto/core/base/Utl.java
+++ src/main/java/ilarkesto/core/base/Utl.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
@@ -186,6 +186,16 @@ public class Utl {
 		int[] ret = new int[list.size()];
 		int i = 0;
 		for (Integer value : list) {
+			ret[i] = value == null ? 0 : value;
+			i++;
+		}
+		return ret;
+	}
+
+	public static float[] toArrayOfFloat(Collection<Float> list) {
+		float[] ret = new float[list.size()];
+		int i = 0;
+		for (Float value : list) {
 			ret[i] = value == null ? 0 : value;
 			i++;
 		}
