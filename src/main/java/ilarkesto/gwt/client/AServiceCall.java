d14ae3ed8c060a3c935fb5f57448177488ac709f
Witoslaw Koczewski <wi@koczewski.de>
2014-08-07 20:19:44 +0200

    GWT Service calls

--- src/main/java/ilarkesto/gwt/client/AServiceCall.java
+++ src/main/java/ilarkesto/gwt/client/AServiceCall.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License along with this program. If not,
  * see <http://www.gnu.org/licenses/>.
  */
@@ -18,7 +18,6 @@ import ilarkesto.core.base.RuntimeTracker;
 import ilarkesto.core.base.Str;
 import ilarkesto.core.base.Utl;
 import ilarkesto.core.logging.Log;
-import ilarkesto.core.service.ServiceCall;
 import ilarkesto.core.time.Tm;
 
 import java.util.ArrayList;
@@ -180,6 +179,9 @@ public abstract class AServiceCall<D extends ADataTransferObject> implements Ser
 
 		if (returnHandler != null) {
 			RuntimeTracker rtHandler = new RuntimeTracker();
+			if (returnHandler instanceof AServiceCallResultHandler) {
+				((AServiceCallResultHandler) returnHandler).setDto(data);
+			}
 			returnHandler.run();
 			runtimeReturnHandler = rtHandler.getRuntime();
 		}
