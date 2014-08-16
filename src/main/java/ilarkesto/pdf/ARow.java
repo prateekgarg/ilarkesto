9043abd2cf9d9f87a53bafe904823621a57b2160
Kacper Grubalski <kacper@grubalski.de>
2014-08-16 15:36:57 +0200

    cell(text,..) is deprecated now.

--- src/main/java/ilarkesto/pdf/ARow.java
+++ src/main/java/ilarkesto/pdf/ARow.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
@@ -54,6 +54,7 @@ public abstract class ARow {
 		return c;
 	}
 
+	@Deprecated
 	public ACell cell(Object text, FontStyle fontStyle) {
 		ACell cell = cell();
 		cell.setFontStyle(fontStyle);
@@ -61,6 +62,7 @@ public abstract class ARow {
 		return cell;
 	}
 
+	@Deprecated
 	public ACell cell(Object text) {
 		ACell cell = cell();
 		cell.text(text);
