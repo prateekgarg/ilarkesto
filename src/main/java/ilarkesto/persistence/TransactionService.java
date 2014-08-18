05a9ee2f34006a49be87093e028439c981b8a22c
Witoslaw Koczewski <wi@koczewski.de>
2014-08-18 14:44:53 +0200

    debug logging

--- src/main/java/ilarkesto/persistence/TransactionService.java
+++ src/main/java/ilarkesto/persistence/TransactionService.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
@@ -112,14 +112,12 @@ public class TransactionService implements IdentifiableResolver<AEntity> {
 
 	public Set<AEntity> getEntities(Predicate<Class> typeFilter, Predicate<AEntity> entityFilter) {
 		Transaction transaction = getCurrentTransaction(false);
-		System.out.println("TransactionService getEntities() in");
 		Set<AEntity> ret;
 		if (transaction == null) {
 			ret = entityStore.getEntities(typeFilter, entityFilter);
 		} else {
 			ret = transaction.getEntities(typeFilter, entityFilter);
 		}
-		System.out.println("TransactionService getEntities() out");
 		return ret;
 	}
 
