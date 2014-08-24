a52399c392303f3b23a182656f46b196aac5abca
Witoslaw Koczewski <wi@koczewski.de>
2014-08-24 11:29:13 +0200

    string util

--- src/main/java/ilarkesto/core/base/Str.java
+++ src/main/java/ilarkesto/core/base/Str.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
@@ -370,6 +370,17 @@ public class Str {
 			}
 		}
 		if (suffix != null) sb.append(suffix);
+		return sb.toString();
+	}
+
+	public static String concat(int[] sa, String delimiter) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < sa.length; i++) {
+			sb.append(sa[i]);
+			if (i < sa.length - 1) {
+				sb.append(delimiter);
+			}
+		}
 		return sb.toString();
 	}
 
