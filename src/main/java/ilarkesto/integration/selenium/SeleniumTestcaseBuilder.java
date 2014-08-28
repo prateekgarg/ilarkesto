1719c15c57f384ee6bc00b16b45ac39a94bef2dd
Witoslaw Koczewski <wi@koczewski.de>
2014-08-28 22:59:05 +0200

    Selenium

--- src/main/java/ilarkesto/integration/selenium/SeleniumTestcaseBuilder.java
+++ src/main/java/ilarkesto/integration/selenium/SeleniumTestcaseBuilder.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- *
+ * 
  * You should have received a copy of the GNU Affero General Public License along with this program. If not,
  * see <http://www.gnu.org/licenses/>.
  */
@@ -22,6 +22,7 @@ public class SeleniumTestcaseBuilder {
 	private String title = "generated-testcase";
 	private String baseUrl;
 	private List<Command> commands = new ArrayList<Command>();
+	private long autoPause = 50l;
 
 	public SeleniumTestcaseBuilder(String baseUrl) {
 		super();
@@ -54,8 +55,13 @@ public class SeleniumTestcaseBuilder {
 		return command("select", target, optionTarget);
 	}
 
-	public Command click(String target) {
-		return command("click", target);
+	public void click(String target) {
+		command("click", target);
+		if (autoPause > 0) pause(autoPause);
+	}
+
+	public Command pause(long millis) {
+		return command("pause", String.valueOf(millis));
 	}
 
 	public Command waitForElementPresent(String target) {
