d227ec3e5013156914e74c5d4a6ed8dc7dcbb9f4
Witoslaw Koczewski <wi@koczewski.de>
2014-09-02 10:14:11 +0200

    Bugfix: compare(Comparable)

--- src/main/java/ilarkesto/core/base/Utl.java
+++ src/main/java/ilarkesto/core/base/Utl.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
@@ -269,7 +269,8 @@ public class Utl {
 		if (a == null && b != null) return -1;
 		if (a != null && b == null) return 1;
 		if (a instanceof String && b instanceof String) return (compare((String) a, (String) b));
-		if (a instanceof Comparable) return ((Comparable) a).compareTo(b);
+		if (a instanceof Comparable && b instanceof Comparable && a.getClass().equals(b.getClass()))
+			return ((Comparable) a).compareTo(b);
 		return compare(a.toString(), b.toString());
 	}
 
