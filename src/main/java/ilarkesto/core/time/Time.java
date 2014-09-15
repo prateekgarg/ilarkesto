e6103a4faf86eb1cb978fd7eea8047eb9075aa0a
Witoslaw Koczewski <wi@koczewski.de>
2014-09-15 09:52:19 +0200

    Time equalsIgnoreSeconds)

--- src/main/java/ilarkesto/core/time/Time.java
+++ src/main/java/ilarkesto/core/time/Time.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
@@ -142,6 +142,10 @@ public class Time implements Comparable<Time>, Serializable, Formatable {
 		if (obj == null) return false;
 		Time other = (Time) obj;
 		return hour == other.hour && minute == other.minute && second == other.second;
+	}
+
+	public final boolean equalsIgnoreSeconds(Time other) {
+		return hour == other.hour && minute == other.minute;
 	}
 
 	public final String toHourMinuteString() {
