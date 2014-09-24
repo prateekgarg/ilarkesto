2d67bda9e20853bee4b4b154d883c801b1432f1b
Witoslaw Koczewski <wi@koczewski.de>
2014-09-24 13:43:49 +0200

    unit test

--- src/test/java/ilarkesto/core/base/UtlTest.java
+++ src/test/java/ilarkesto/core/base/UtlTest.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
@@ -16,11 +16,24 @@ package ilarkesto.core.base;
 
 import ilarkesto.testng.ATest;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.testng.annotations.Test;
 
 public class UtlTest extends ATest {
+
+	@Test
+	public void sortReverse() {
+		List<String> list = new ArrayList<String>();
+		list.add("c");
+		list.add("a");
+		list.add("b");
+		Utl.sortReverse(list);
+		assertEquals(list.get(0), "c");
+		assertEquals(list.get(1), "b");
+		assertEquals(list.get(2), "a");
+	}
 
 	@Test
 	public void moveElementUp() {
