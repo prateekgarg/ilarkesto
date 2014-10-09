c98daf17fe86e7ecc726c5fe3c2a25058df42894
Witoslaw Koczewski <wi@koczewski.de>
2014-10-09 20:58:07 +0200

    Bugfix: BigDecimal scale rounding

--- src/main/java/ilarkesto/core/localization/Localizer.java
+++ src/main/java/ilarkesto/core/localization/Localizer.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License along with this program. If not,
  * see <http://www.gnu.org/licenses/>.
  */
@@ -74,7 +74,7 @@ public class Localizer {
 		if (value instanceof BigDecimal) {
 			BigDecimal bd = (BigDecimal) value;
 			if (scale != null && bd.scale() != scale) {
-				bd = bd.setScale(scale);
+				bd = bd.setScale(scale, BigDecimal.ROUND_HALF_UP);
 			}
 			ret = bd.toPlainString();
 		} else {
