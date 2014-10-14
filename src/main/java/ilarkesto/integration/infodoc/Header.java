29d85c48f9bb23f2edf3d9f834e3e3b171c32acf
Witoslaw Koczewski <wi@koczewski.de>
2014-10-14 08:34:55 +0200

    InfoDoc header with reference

--- src/main/java/ilarkesto/integration/infodoc/Header.java
+++ src/main/java/ilarkesto/integration/infodoc/Header.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- *
+ * 
  * You should have received a copy of the GNU Affero General Public License along with this program. If not,
  * see <http://www.gnu.org/licenses/>.
  */
@@ -37,6 +37,7 @@ public class Header extends AInfoDocElement {
 			sb.append(context.getIndentationPrefix(this)).append(Str.toHtml(text, true));
 		} else {
 			String title = referenceResolver.getTitle(ref);
+			if (Str.isBlank(title)) title = "@" + ref;
 			String href = context.getHref(ref);
 			sb.append(context.getIndentationPrefix(this)).append("&nbsp;<a href='").append(href)
 					.append("' style='color:" + context.getColor(getDepth()) + ";'>").append(Str.toHtml(title, true))
