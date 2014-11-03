257f2ac5e7809ac6e22fd4464c26ec50c6e49975
Witoslaw Koczewski <wi@koczewski.de>
2014-11-03 17:09:40 +0100

    GWT runtime logging

--- src/main/java/ilarkesto/gwt/server/AGwtConversation.java
+++ src/main/java/ilarkesto/gwt/server/AGwtConversation.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
@@ -148,7 +148,7 @@ public abstract class AGwtConversation<S extends AWebSession, E extends Transfer
 		TransferBus transferBus = new TransferBus();
 		RuntimeTracker rt = new RuntimeTracker();
 		transferBus.add(entity);
-		if (!transferBusWarningPosted && rt.getRuntime() > 200) {
+		if (!transferBusWarningPosted && rt.getRuntime() > 50) {
 			log.warn("Collecting passengers took", rt.getRuntimeFormated(), "->", toString(entity), "->",
 				transferBus.getEntities());
 			transferBusWarningPosted = true;
