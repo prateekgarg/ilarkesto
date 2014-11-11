bd4dac9f63b54c5f7571db815ff25833626ef08e
Witoslaw Koczewski <wi@koczewski.de>
2014-11-11 14:22:02 +0100

    Money

--- src/main/java/ilarkesto/core/base/MultilineBuilder.java
+++ src/main/java/ilarkesto/core/base/MultilineBuilder.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- *
+ * 
  * You should have received a copy of the GNU Affero General Public License along with this program. If not,
  * see <http://www.gnu.org/licenses/>.
  */
@@ -18,12 +18,19 @@ public class MultilineBuilder {
 
 	private StringBuilder sb = new StringBuilder();
 
+	public MultilineBuilder lnIfAllNotNull(Object... words) {
+		for (Object o : words) {
+			if (o == null) return this;
+		}
+		return ln(words);
+	}
+
 	public MultilineBuilder ln(Object... words) {
 		boolean added = false;
 		boolean first = true;
 		for (Object o : words) {
 			if (o == null) continue;
-			String word = o.toString();
+			String word = Str.format(o);
 			if (Str.isBlank(word)) continue;
 			if (first) {
 				first = false;
--- src/main/java/ilarkesto/core/money/Money.java
+++ src/main/java/ilarkesto/core/money/Money.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
@@ -135,6 +135,15 @@ public final class Money implements Comparable<Money>, Serializable, Formatable 
 
 	public Money substract(Money... subtrahends) throws MultipleCurrenciesException {
 		return computeDifference(this, subtrahends);
+	}
+
+	public Money subtractPercent(BigDecimal percent) {
+		if (percent == null) return this;
+		try {
+			return substract(percent(percent));
+		} catch (MultipleCurrenciesException ex) {
+			throw new RuntimeException(ex);
+		}
 	}
 
 	public Money add(Money summand) throws MultipleCurrenciesException {
