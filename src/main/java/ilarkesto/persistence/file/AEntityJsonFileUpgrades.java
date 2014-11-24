cbdba27450dfb2230165024aa7e1466b8bb7696a
Witoslaw Koczewski <wi@koczewski.de>
2014-11-24 07:59:30 +0100

    JSON persistennce

---
+++ src/main/java/ilarkesto/persistence/file/AEntityJsonFileUpgrades.java
@@ -0,0 +1,204 @@
+/*
+ * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with this program. If not,
+ * see <http://www.gnu.org/licenses/>.
+ */
+package ilarkesto.persistence.file;
+
+import ilarkesto.core.base.Utl;
+import ilarkesto.core.logging.Log;
+import ilarkesto.core.persistance.AEntity;
+import ilarkesto.core.time.Date;
+import ilarkesto.core.time.DateAndTime;
+import ilarkesto.core.time.DateRange;
+import ilarkesto.io.IO;
+import ilarkesto.json.JsonObject;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public abstract class AEntityJsonFileUpgrades {
+
+	protected final Log log = Log.get(getClass());
+
+	private List<FileUpgrader> upgraders = new ArrayList<FileUpgrader>();
+
+	protected abstract void initialize();
+
+	public AEntityJsonFileUpgrades() {
+		initialize();
+	}
+
+	protected boolean convertDateAndTimeToDate(JsonObject json, String... properties) {
+		boolean converted = false;
+		for (String property : properties) {
+			String value = json.getString(property);
+			if (value == null) continue;
+			try {
+				new Date(value);
+			} catch (IllegalArgumentException ex) {
+				String newValue = new DateAndTime(value).getDate().toString();
+				json.put(property, newValue);
+				converted = true;
+			}
+		}
+		return converted;
+	}
+
+	protected boolean convertToDateRange(JsonObject json, String oldStartProperty, String oldEndProperty,
+			String newRangeProperty) {
+		if (!json.contains(oldStartProperty) && !json.contains(oldEndProperty)) return false;
+
+		String sStart = json.getString(oldStartProperty);
+		Date start = sStart == null ? Date.today() : new Date(sStart);
+		String sEnd = json.getString(oldEndProperty);
+		Date end = sEnd == null ? Date.today() : new Date(sEnd);
+		DateRange dateRange;
+		try {
+			dateRange = new DateRange(start, end);
+		} catch (Exception ex) {
+			log.error("DateRange-Upgrade mit ungültigem Zeitraum:", json.toFormatedString());
+			dateRange = new DateRange(start);
+		}
+
+		json.put(newRangeProperty, dateRange.toString());
+		json.remove(oldStartProperty);
+		json.remove(oldEndProperty);
+
+		return true;
+	}
+
+	protected boolean rename(JsonObject json, String fromFieldname, String toFieldname) {
+		if (json.contains(fromFieldname)) {
+			Object value = json.get(fromFieldname);
+			json.remove(fromFieldname);
+			json.put(toFieldname, value);
+			return true;
+		}
+		return false;
+	}
+
+	protected boolean remove(JsonObject json, String... fields) {
+		boolean removed = false;
+		for (String field : fields) {
+			if (json.contains(field)) {
+				json.remove(field);
+				removed = true;
+			}
+		}
+		return removed;
+	}
+
+	public int getSoftwareVersion() {
+		int version = 0;
+		for (FileUpgrader upgrader : upgraders) {
+			version = Math.max(version, upgrader.getChangeInsertionVersion());
+		}
+		return version;
+	}
+
+	public void upgradeEntitiesDir(File dir, int fileVersion) {
+		Collections.sort(upgraders);
+
+		if (fileVersion < 18) {
+			File mediaplanStatuss = new File(dir.getPath() + "/MediaplanStatus");
+			if (mediaplanStatuss.exists()) IO.delete(mediaplanStatuss);
+		}
+		if (fileVersion < 5) {
+			File werbetraegerinfos = new File(dir.getPath() + "/Werbetraegerinfo");
+			if (werbetraegerinfos.exists()) IO.move(werbetraegerinfos, new File(dir.getPath() + "/Werbetraeger"));
+		}
+	}
+
+	public void upgradeEntity(File file, Class<? extends AEntity> entityType, int fileVersion) {
+		for (FileUpgrader upgrader : upgraders) {
+			if (fileVersion < upgrader.getChangeInsertionVersion() && entityType.equals(upgrader.getEntityType()))
+				upgrader.upgrade(file, fileVersion);
+
+		}
+	}
+
+	public abstract class JsonUpgrader<E extends AEntity> extends FileUpgrader<E> {
+
+		protected File entityFile;
+
+		public JsonUpgrader(Class<E> entityType, int changeInsertionVersion) {
+			super(entityType, changeInsertionVersion);
+		}
+
+		@Override
+		public final void upgrade(File file, int fileVersion) {
+			this.entityFile = file;
+			JsonObject json = JsonObject.loadFile(file, false);
+			boolean changed;
+			try {
+				changed = upgrade(json);
+			} catch (Exception ex) {
+				throw new RuntimeException("Upgrading JSON file failed: " + file, ex);
+			}
+			if (!changed) return;
+			log.info("Entity JSON file upgraded:", file);
+			json.save();
+		}
+
+		protected abstract boolean upgrade(JsonObject json);
+
+		protected final boolean upgradeTimeAndUser(JsonObject json, String property) {
+			String refPropertyName = property + "Id";
+			String userAndTimeId = json.getString(refPropertyName);
+			if (userAndTimeId == null) return remove(json, refPropertyName);
+
+			File userAndTimeFile = new File(entityFile.getParentFile().getParentFile().getPath() + "/UserAndTime/"
+					+ userAndTimeId + ".json");
+
+			JsonObject userAndTimeJson = JsonObject.loadFile(userAndTimeFile, false);
+
+			json.put(property, userAndTimeJson.getString("userId") + " " + userAndTimeJson.getString("time"));
+
+			remove(json, refPropertyName);
+			return true;
+		}
+
+	}
+
+	public abstract class FileUpgrader<E extends AEntity> implements Comparable<FileUpgrader<E>> {
+
+		private Class<E> entityType;
+		private int changeInsertionVersion;
+
+		public FileUpgrader(Class<E> entityType, int changeInsertionVersion) {
+			super();
+			this.entityType = entityType;
+			this.changeInsertionVersion = changeInsertionVersion;
+			upgraders.add(this);
+		}
+
+		public abstract void upgrade(File file, int fileVersion);
+
+		public Class<E> getEntityType() {
+			return entityType;
+		}
+
+		public final int getChangeInsertionVersion() {
+			return changeInsertionVersion;
+		}
+
+		@Override
+		public final int compareTo(FileUpgrader<E> o) {
+			return Utl.compare(changeInsertionVersion, o.changeInsertionVersion);
+		}
+
+	}
+
+}
---
+++ src/main/java/ilarkesto/persistence/file/AJsonFilesEntityDatabase.java
@@ -0,0 +1,224 @@
+/*
+ * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
+ * 
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License along with this program. If not,
+ * see <http://www.gnu.org/licenses/>.
+ */
+package ilarkesto.persistence.file;
+
+import ilarkesto.base.Bytes;
+import ilarkesto.base.Env;
+import ilarkesto.core.base.RuntimeTracker;
+import ilarkesto.core.persistance.ACachingEntityDatabase;
+import ilarkesto.core.persistance.AEntity;
+import ilarkesto.core.persistance.Transaction;
+import ilarkesto.di.Context;
+import ilarkesto.di.app.AApplication;
+import ilarkesto.integration.git.Git;
+import ilarkesto.integration.git.GitProject;
+import ilarkesto.io.AFileStorage;
+import ilarkesto.io.IO;
+import ilarkesto.json.JsonMapper;
+import ilarkesto.json.JsonMapper.TypeResolver;
+import ilarkesto.webapp.AWebApplication;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+// TODO background task which checks if transactions are open (running) too long
+public abstract class AJsonFilesEntityDatabase extends ACachingEntityDatabase {
+
+	private AFileStorage storage;
+	private GitProject git;
+
+	private ThreadLocal<Transaction> tlTransaction = new ThreadLocal<Transaction>();
+	private Collection<Transaction> transactions = Collections.synchronizedCollection(new ArrayList<Transaction>());
+	private int transactionNumberCounter = 0;
+
+	protected abstract AEntityJsonFileUpgrades createUpgrader();
+
+	protected abstract List<Class<? extends ilarkesto.core.persistance.AEntity>> getEntityTypes();
+
+	protected abstract TypeResolver createTypeResolver();
+
+	public AJsonFilesEntityDatabase() {
+		AApplication application = AApplication.get();
+		storage = application.getFileStorage().getSubStorage("entities");
+		git = new GitProject(new Git(), new File(application.getApplicationDataDir()));
+		load();
+	}
+
+	private void load() {
+		int dataVersion = loadVersion();
+		AEntityJsonFileUpgrades upgrader = createUpgrader();
+		int softwareVersion = upgrader.getSoftwareVersion();
+
+		if (dataVersion > softwareVersion)
+			throw new IllegalStateException("Data version " + dataVersion + " is bigger then softwareVersion "
+					+ softwareVersion);
+
+		log.info("Loading entities from", storage, "| data-version", dataVersion, "| software-version", softwareVersion);
+		RuntimeTracker rt = new RuntimeTracker();
+		upgrader.upgradeEntitiesDir(storage.getFile(null), dataVersion);
+		TypeResolver typeResolver = createTypeResolver();
+		for (Class<? extends AEntity> type : getEntityTypes()) {
+			int count = 0;
+			File dir = storage.getFile(type.getSimpleName());
+			File[] files = dir.listFiles();
+			if (files != null) {
+				for (File file : files) {
+					if (!file.isFile()) continue;
+					if (!file.getName().endsWith(".json")) continue;
+					upgrader.upgradeEntity(file, type, dataVersion);
+					if (!file.exists()) continue;
+					AEntity entity;
+					try {
+						entity = JsonMapper.deserialize(file, type, typeResolver);
+					} catch (Exception ex) {
+						throw new RuntimeException("Loading entity failed: " + file, ex);
+					}
+					cache.add(entity);
+					count++;
+				}
+			}
+			log.info("   ", type.getSimpleName(), count);
+		}
+
+		saveVersion(softwareVersion);
+
+		log.info(cache.size(), "entities loaded in", rt.getRuntimeFormated());
+
+	}
+
+	private void saveVersion(int version) {
+		IO.writeFile(getVersionFile(), String.valueOf(version), IO.UTF_8);
+	}
+
+	private int loadVersion() {
+		File versionFile = getVersionFile();
+		if (!versionFile.exists()) return 0;
+		return Integer.parseInt(IO.readFile(versionFile, IO.UTF_8).trim());
+	}
+
+	private File getVersionFile() {
+		return storage.getFile("version.txt");
+	}
+
+	@Override
+	protected synchronized void onUpdate(Collection<AEntity> modified, Collection<String> deleted,
+			Map<String, Map<String, String>> modifiedPropertiesByEntityIds, Runnable callback) {
+		if ((modified == null || modified.isEmpty()) && (deleted == null || deleted.isEmpty())) return;
+		List<File> files = new ArrayList<File>();
+		RuntimeTracker rt = new RuntimeTracker();
+		int saveCount = 0;
+		Collection<AEntity> created = new ArrayList<AEntity>();
+		if (modified != null) {
+			for (AEntity entity : modified) {
+				File file = getFile(entity);
+				if (!file.exists()) {
+					created.add(entity);
+				}
+				files.add(file);
+				log.debug("Saving entity:", entity.getClass().getSimpleName(), file.getName(), "in", file.getParent());
+				try {
+					JsonMapper.serialize(entity, file);
+				} catch (IOException ex) {
+					throw new RuntimeException("Writing entity to file failed: " + file + " -> " + entity, ex);
+				}
+				saveCount++;
+			}
+		}
+		int deleteCount = 0;
+		if (deleted != null) {
+			for (String id : deleted) {
+				AEntity entity = cache.get(id);
+				File file = getFile(entity);
+				files.add(file);
+				log.debug("Deleting entity", entity.getClass().getSimpleName(), file);
+				IO.delete(file);
+				deleteCount++;
+			}
+		}
+		log.info("Entity changes saved:", rt.getRuntimeFormated(), "(" + saveCount, "saved,", deleteCount, "deleted)");
+
+		if (git.isInitialized()) {
+			git.addAll();
+			git.commit(Context.get().toString());
+		}
+
+		AWebApplication webApplication = AWebApplication.get();
+		webApplication.deleteFromClients(deleted);
+		webApplication.sendToAllIfTracking((Collection) modified);
+		webApplication.sendToAll((Collection) created);
+
+		if (callback != null) callback.run();
+	}
+
+	private File getFile(AEntity entity) {
+		return storage.getFile(entity.getClass().getSimpleName() + "/" + entity.getId() + ".json");
+	}
+
+	@Override
+	public synchronized Transaction getTransaction() {
+		Transaction transaction = tlTransaction.get();
+		if (transaction == null) {
+			int no = ++transactionNumberCounter;
+			transaction = new Transaction(this, "#" + no + " (" + Thread.currentThread().getName() + ")", false, true);
+			transactions.add(transaction);
+			tlTransaction.set(transaction);
+		}
+		return transaction;
+	}
+
+	@Override
+	public void onTransactionFinished(Transaction transaction) {
+		Transaction current = tlTransaction.get();
+		if (current == null) {
+			if (transaction == null) return;
+			throw new IllegalStateException("Current transaction == null. Finished: " + transaction);
+		}
+		if (current != transaction)
+			throw new IllegalStateException("Transaction is not current transaction: " + transaction);
+		tlTransaction.set(null);
+		transactions.remove(transaction);
+	}
+
+	@Override
+	public boolean isTransactionWithChangesOpen() {
+		for (Transaction transaction : transactions) {
+			if (!transaction.isEmpty()) return true;
+		}
+		return false;
+	}
+
+	@Override
+	public String createInfo() {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("\nEntities size on disk: ")
+				.append(new Bytes(Env.get().getFileSize(storage.getFile(null))).toRoundedString()).append("\n");
+
+		sb.append("\nEntity counts:\n");
+		for (Map.Entry<Class, Integer> entry : cache.countEntities().entrySet()) {
+			sb.append("* ").append(entry.getKey().getSimpleName()).append(": ").append(entry.getValue()).append("\n");
+		}
+
+		if (git.isInitialized()) sb.append("\nGit status: ").append(git.status());
+
+		return sb.toString();
+	}
+
+}
