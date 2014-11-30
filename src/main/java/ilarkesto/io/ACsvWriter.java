944690b53064fe2987ac6e70a38be3745f64981b
Witoslaw Koczewski <wi@koczewski.de>
2014-11-30 11:55:35 +0100

    CSV-Export

--- src/main/java/ilarkesto/io/ACsvWriter.java
+++ src/main/java/ilarkesto/io/ACsvWriter.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- *
+ * 
  * You should have received a copy of the GNU Affero General Public License along with this program. If not,
  * see <http://www.gnu.org/licenses/>.
  */
@@ -108,7 +108,15 @@ public abstract class ACsvWriter<R> {
 		public abstract Object getValue(R record);
 
 		public String getCsvValue(R record) {
-			return Str.format(getValue(record));
+			String ret = Str.format(getValue(record));
+			if (ret == null) return null;
+			int lengthAutocut = getLengthAutocut();
+			if (lengthAutocut > 0 && ret.length() > lengthAutocut) ret = Str.cutRight(ret, lengthAutocut);
+			return ret;
+		}
+
+		protected int getLengthAutocut() {
+			return -1;
 		}
 
 	}
