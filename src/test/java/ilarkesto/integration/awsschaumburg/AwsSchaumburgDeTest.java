21f4248c503195a53330e943aada3a57acb2d7ae
Witoslaw Koczewski <wi@koczewski.de>
2015-01-02 12:50:09 +0100

    Unit-Test

--- src/test/java/ilarkesto/integration/awsschaumburg/AwsSchaumburgDeTest.java
+++ src/test/java/ilarkesto/integration/awsschaumburg/AwsSchaumburgDeTest.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License along with this program. If not,
  * see <http://www.gnu.org/licenses/>.
  */
@@ -42,7 +42,7 @@ public class AwsSchaumburgDeTest extends ATest {
 	public void loadPickupAreas() throws ParseException {
 		List<WastePickupArea> areas = AwsSchaumburgDe.loadPickupAreas();
 		log.info(areas);
-		assertSize(areas, 230);
+		assertSize(areas, 231);
 	}
 
 }
