c660d6d79eef60fe4a3d29d5e3853855418a17a5
Witoslaw Koczewski <wi@koczewski.de>
2015-02-15 18:48:08 +0100

    OAuth2 ; GoogleClient

---
+++ src/main/java/ilarkesto/integration/google/GoogleClient.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
+ * 
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License along with this program. If not,
+ * see <http://www.gnu.org/licenses/>.
+ */
+package ilarkesto.integration.google;
+
+import ilarkesto.core.auth.LoginData;
+import ilarkesto.core.base.Str;
+import ilarkesto.core.logging.Log;
+import ilarkesto.integration.oauth.OAuth2;
+import ilarkesto.swing.LoginPanel;
+
+import java.io.File;
+
+public class GoogleClient extends OAuth2 {
+
+	public static void main(String[] args) {
+		LoginData clientIdLogin = LoginPanel.showDialog(null, "Client ID", new File(
+				"runtimedata/google-oauth2.properties"));
+		if (clientIdLogin == null) return;
+
+		GoogleClient client = new GoogleClient(clientIdLogin.getLogin(), clientIdLogin.getPassword(), REDIRECT_OOB,
+				null, SCOPE_USERINFO_EMAIL, SCOPE_CONTACTS);
+		Log.TEST(client.createUrlForAuthenticationRequest());
+
+		LoginData authorizationCodeLogin = LoginPanel.showDialog(null, "Authorization Code", new File(
+				"runtimedata/google-oauth2-code.properties"));
+
+		client.exchangeAuthorizationCodeForAccessToken(authorizationCodeLogin.getPassword());
+
+		client.exchangeRefreshTokenForAccessToken();
+
+	}
+
+	public static final String SCOPE_USERINFO_EMAIL = "https://www.googleapis.com/auth/userinfo.email";
+	public static final String SCOPE_USERINFO_PROFILE = "https://www.googleapis.com/auth/userinfo.profile";
+	public static final String SCOPE_CALENDAR = "https://www.googleapis.com/auth/calendar";
+	public static final String SCOPE_CONTACTS = "https://www.google.com/m8/feeds/";
+
+	public GoogleClient(String clientId, String clientSecret, String redirectUri, String refreshToken, String... scopes) {
+		super("https://accounts.google.com/o/oauth2/auth", "https://www.googleapis.com/oauth2/v3/token", clientId,
+				clientSecret, redirectUri, refreshToken, concatScope(scopes));
+	}
+
+	private static String concatScope(String[] scopes) {
+		return Str.concat(scopes, "+");
+	}
+
+}
---
+++ src/main/java/ilarkesto/integration/oauth/OAuth2.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
+ * 
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License along with this program. If not,
+ * see <http://www.gnu.org/licenses/>.
+ */
+package ilarkesto.integration.oauth;
+
+import ilarkesto.core.base.Str;
+import ilarkesto.core.logging.Log;
+import ilarkesto.json.JsonObject;
+import ilarkesto.net.ApacheHttpDownloader;
+
+import java.util.HashMap;
+
+public class OAuth2 {
+
+	private final Log log = Log.get(getClass());
+
+	public static final String REDIRECT_OOB = "urn:ietf:wg:oauth:2.0:oob";
+
+	private String authEndpoint;
+	private String tokenEndpoint;
+	private String clientId;
+	private String clientSecret;
+	private String redirectUri;
+	private String scope;
+	private String refreshToken;
+
+	private String accessToken;
+
+	public OAuth2(String authEndpoint, String tokenEndpoint, String clientId, String clientSecret, String redirectUri,
+			String refreshToken, String scope) {
+		super();
+		this.authEndpoint = authEndpoint;
+		this.tokenEndpoint = tokenEndpoint;
+		this.clientId = clientId;
+		this.clientSecret = clientSecret;
+		this.redirectUri = redirectUri;
+		this.refreshToken = refreshToken;
+		this.scope = scope;
+	}
+
+	public String createUrlForAuthenticationRequest() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(authEndpoint).append("?");
+		sb.append("scope=").append(Str.encodeUrlParameter(scope)).append("&");
+		sb.append("redirect_uri=").append(Str.encodeUrlParameter(redirectUri)).append("&");
+		sb.append("client_id=").append(clientId).append("&");
+		sb.append("response_type=code&");
+		sb.append("include_granted_scopes=true");
+		String url = sb.toString();
+		log.info("Authentication request URL:", url);
+		return url;
+	}
+
+	public void exchangeRefreshTokenForAccessToken() {
+		if (Str.isBlank(refreshToken)) throw new IllegalArgumentException("refresh token not set");
+
+		HashMap<String, String> params = new HashMap<String, String>();
+		params.put("refresh_token", refreshToken);
+		params.put("client_id", clientId);
+		params.put("client_secret", clientSecret);
+		params.put("grant_type", "refresh_token");
+		ApacheHttpDownloader http = new ApacheHttpDownloader();
+		String result = http.post(tokenEndpoint, params, null);
+
+		JsonObject json = JsonObject.parse(result);
+
+		accessToken = json.getString("access_token");
+		if (Str.isBlank(accessToken))
+			throw new RuntimeException(
+					"OAuth: Exchanging refresh token for access token failed. Missing access_token: " + result);
+
+		log.info("Refresh token exchanged for access token");
+	}
+
+	public void exchangeAuthorizationCodeForAccessToken(String authorizationCode) {
+		if (Str.isBlank(authorizationCode)) throw new IllegalArgumentException("authorizationCode required");
+
+		HashMap<String, String> params = new HashMap<String, String>();
+		params.put("code", authorizationCode);
+		params.put("client_id", clientId);
+		params.put("client_secret", clientSecret);
+		params.put("grant_type", "authorization_code");
+		params.put("redirect_uri", redirectUri);
+		ApacheHttpDownloader http = new ApacheHttpDownloader();
+		String result = http.post(tokenEndpoint, params, null);
+
+		JsonObject json = JsonObject.parse(result);
+
+		accessToken = json.getString("access_token");
+		if (Str.isBlank(accessToken))
+			throw new RuntimeException(
+					"OAuth: Exchanging authorization code for access+refresh token failed. Missing access_token: "
+							+ result);
+
+		refreshToken = json.getString("refresh_token");
+		if (Str.isBlank(refreshToken))
+			throw new RuntimeException(
+					"OAuth: Exchanging authorization code for access/refresh token failed. Missing refresh_token: "
+							+ result);
+
+		log.info("Authorization code exchanged for access token and refresh token");
+	}
+
+	public String getRefreshToken() {
+		return refreshToken;
+	}
+
+}
