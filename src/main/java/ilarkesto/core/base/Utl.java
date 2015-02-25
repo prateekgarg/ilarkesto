87bfbaed9f7414865dda497aaa9fca1405599630
Witoslaw Koczewski <wi@koczewski.de>
2015-02-25 18:23:41 +0100

    utils

--- src/main/java/ilarkesto/core/base/Utl.java
+++ src/main/java/ilarkesto/core/base/Utl.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
@@ -33,6 +33,24 @@ import java.util.Set;
 public class Utl {
 
 	public static final BigDecimal BD_HUNDRED = new BigDecimal(100);
+
+	public static Long biggest(Long... values) {
+		Long biggest = null;
+		for (Long value : values) {
+			if (value == null) continue;
+			if (biggest == null || value.intValue() > biggest.intValue()) biggest = value;
+		}
+		return biggest;
+	}
+
+	public static Long biggest(Iterable<Long> values) {
+		Long biggest = null;
+		for (Long value : values) {
+			if (value == null) continue;
+			if (biggest == null || value.longValue() > biggest.longValue()) biggest = value;
+		}
+		return biggest;
+	}
 
 	public static boolean contains(int[] array, int value) {
 		for (int i = 0; i < array.length; i++) {
--- src/main/java/ilarkesto/core/time/DateAndTime.java
+++ src/main/java/ilarkesto/core/time/DateAndTime.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
@@ -200,6 +200,24 @@ public class DateAndTime implements Comparable<DateAndTime>, Serializable, Forma
 
 	public static DateAndTime now() {
 		return new DateAndTime();
+	}
+
+	public static DateAndTime latest(Iterable<DateAndTime> dates) {
+		DateAndTime latest = null;
+		for (DateAndTime date : dates) {
+			if (date == null) continue;
+			if (latest == null || date.isAfter(latest)) latest = date;
+		}
+		return latest;
+	}
+
+	public static DateAndTime latest(DateAndTime... dates) {
+		DateAndTime latest = null;
+		for (DateAndTime date : dates) {
+			if (date == null) continue;
+			if (latest == null || date.isAfter(latest)) latest = date;
+		}
+		return latest;
 	}
 
 }
