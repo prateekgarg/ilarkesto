4fa935cdef319de83719673c42df800e77585f78
Witoslaw Koczewski <wi@koczewski.de>
2015-02-25 14:02:28 +0100

    CSV-Parser

--- src/main/java/ilarkesto/core/money/Money.java
+++ src/main/java/ilarkesto/core/money/Money.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
@@ -171,6 +171,10 @@ public final class Money implements Comparable<Money>, Serializable, Formatable 
 	@Deprecated
 	public Money divideAndRound(float divisor) {
 		return new Money(0, Math.round((cent / divisor)), currency);
+	}
+
+	public Money divideAndRound(BigDecimal divisor) {
+		return new Money(getAmount().divide(divisor, BigDecimal.ROUND_HALF_UP), currency);
 	}
 
 	public String getAmountAsString(char decimalSeparator) {
--- src/main/java/ilarkesto/io/CsvParser.java
+++ src/main/java/ilarkesto/io/CsvParser.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
@@ -77,8 +77,8 @@ public final class CsvParser {
 			c = readNextChar();
 			if (quoted && c == '"') {
 				int cNext = readNextChar();
-				if (cNext == '"') {
-					sb.append('"');
+				if (!isSeperator(cNext) && !isEOL(cNext)) {
+					sb.append((char) cNext);
 					continue;
 				} else {
 					resetLastChar();
@@ -87,7 +87,7 @@ public final class CsvParser {
 			}
 			if (isEOL(c)) {
 				if (quoted) {
-					if (c == -1) throw new ParseException("Unexpected OEF in field");
+					if (c == -1) throw new ParseException("Unexpected OEL in field");
 				} else {
 					resetLastChar();
 					return sb.toString();
