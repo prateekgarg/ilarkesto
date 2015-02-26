e228f9800f51759a74d9a666d2059092af1be020
Witoslaw Koczewski <wi@koczewski.de>
2015-02-26 12:35:41 +0100

    google contacts api

--- src/main/java/ilarkesto/integration/google/Google.java
+++ src/main/java/ilarkesto/integration/google/Google.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
@@ -326,11 +326,10 @@ public class Google {
 			value = value.trim();
 			if (Str.isBlank(value)) value = null;
 		}
-		if (value != null) {}
 		for (UserDefinedField field : contact.getUserDefinedFields()) {
 			if (name.equals(field.getKey())) {
-				field.setValue(value);
-				return;
+				contact.removeExtension(field);
+				break;
 			}
 		}
 		if (value == null) return;
@@ -562,7 +561,7 @@ public class Google {
 	}
 
 	public static Organization createOrganization(String name, String jobTitle) {
-		if (name == null) return null;
+		if (Str.isBlank(name)) return null;
 		Organization org = new Organization();
 		org.setLabel(name);
 		org.setOrgName(new OrgName(name, null));
