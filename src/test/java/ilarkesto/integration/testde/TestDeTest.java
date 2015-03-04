60938f90d0d2b35c39f96843cd26336e3763bdfb
Witoslaw Koczewski <wi@koczewski.de>
2015-03-04 19:05:00 +0100

    unit tests

--- src/test/java/ilarkesto/integration/testde/TestDeTest.java
+++ src/test/java/ilarkesto/integration/testde/TestDeTest.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- *
+ * 
  * You should have received a copy of the GNU Affero General Public License along with this program. If not,
  * see <http://www.gnu.org/licenses/>.
  */
@@ -205,7 +205,7 @@ public class TestDeTest extends ATest {
 		assertContains(summary, "Ob Beutels­auger oder Sauger mit Staubbox");
 		assertContains(summary, "Einem Gerät reichen bereits 870 Watt für gute Saug­ergeb­nisse.");
 
-		assertSize(subArticles, 11);
+		assertSize(subArticles, 10);
 
 		SubArticleRef subArticleStaubsauger = subArticles.get(9);
 		assertEquals(subArticleStaubsauger.getTitle(), "Staubsauger");
