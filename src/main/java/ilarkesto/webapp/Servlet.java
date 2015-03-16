19f750a81f49018b063308d20becdb56b76d2c19
Witoslaw Koczewski <wi@koczewski.de>
2015-03-16 11:59:31 +0100

    refactoring web application

--- src/main/java/ilarkesto/webapp/Servlet.java
+++ src/main/java/ilarkesto/webapp/Servlet.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
@@ -45,6 +45,22 @@ public abstract class Servlet {
 	public static final String HTTP_DATE_FORMAT = "EEE, dd MMM yyyy, HH:mm:";
 
 	private Servlet() {}
+
+	public static void sendErrorForbidden(HttpServletResponse response) {
+		sendError(response, HttpServletResponse.SC_FORBIDDEN, null);
+	}
+
+	public static void sendError(HttpServletResponse response, int errorCode, String message) {
+		try {
+			if (Str.isBlank(message)) {
+				response.sendError(errorCode);
+			} else {
+				response.sendError(errorCode, message);
+			}
+		} catch (IOException ex) {
+			log.info("Sending HTTP error failed:", response, ex);
+		}
+	}
 
 	public static String readContentToString(HttpServletRequest request) {
 		BufferedReader in;
