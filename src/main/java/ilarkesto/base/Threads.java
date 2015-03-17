672fef163c46a3a73166bccab8fd797031f4a9bb
Witoslaw Koczewski <wi@koczewski.de>
2015-03-17 12:38:30 +0100

    thread identification

--- src/main/java/ilarkesto/base/Threads.java
+++ src/main/java/ilarkesto/base/Threads.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License along with this program. If not,
  * see <http://www.gnu.org/licenses/>.
  */
@@ -107,6 +107,7 @@ public class Threads {
 				return "TOMCAT";
 			if (call.contains("org.apache.tomcat.util.net.JIoEndpoint$Acceptor.run()")) return "TOMCAT";
 			if (call.contains("org.apache.catalina.core.StandardServer.await()")) return "TOMCAT";
+			if (call.contains("org.apache.tomcat.util.net.JIoEndpoint$AsyncTimeout.run()")) return "TOMCAT";
 			if (call.contains("javax.servlet.http.HttpServlet.service()")) return "HTTP";
 
 			// ILARKESTO
