8bd4590a423f16e4bfbcba69a9ad3524fc09cde8
Witoslaw Koczewski <wi@koczewski.de>
2015-03-23 11:59:42 +0100

    delete empty dirs

--- src/main/java/ilarkesto/io/IO.java
+++ src/main/java/ilarkesto/io/IO.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
@@ -800,9 +800,18 @@ public abstract class IO {
 	}
 
 	public static void deleteQuiet(File f) {
+		deleteQuiet(f, false);
+	}
+
+	public static void deleteQuiet(File f, boolean deleteEmptyDirs) {
 		try {
 			IO.delete(f);
 		} catch (Throwable ex) {}
+		if (deleteEmptyDirs) {
+			File dir = f.getParentFile();
+			String[] files = dir.list();
+			if (files == null || files.length == 0) IO.deleteQuiet(dir, deleteEmptyDirs);
+		}
 	}
 
 	public static void copyDataToFile(byte[] data, File file) {
