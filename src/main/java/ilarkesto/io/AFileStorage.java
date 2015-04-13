2cb85f5ead9083af8d0a3694503420e14fb71fd7
Witoslaw Koczewski <wi@koczewski.de>
2015-04-13 12:01:02 +0200

    file storage

--- src/main/java/ilarkesto/io/AFileStorage.java
+++ src/main/java/ilarkesto/io/AFileStorage.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- *
+ * 
  * You should have received a copy of the GNU Affero General Public License along with this program. If not,
  * see <http://www.gnu.org/licenses/>.
  */
@@ -44,6 +44,35 @@ public abstract class AFileStorage {
 			ret.add(file);
 		}
 		return ret;
+	}
+
+	public final Set<String> listFilenames(boolean includeSubdirectories, boolean listDirecotries) {
+		Set<String> ret = new HashSet<String>();
+		File baseDir = getBaseDir();
+		if (baseDir == null) return ret;
+		String sub = "";
+		if (!Str.isBlank(path)) sub += "/" + path;
+		File dir = new File(baseDir.getPath() + sub);
+		addFilenames(ret, dir, null, includeSubdirectories, listDirecotries);
+		return ret;
+	}
+
+	private void addFilenames(Set<String> ret, File dir, String prefix, boolean includeSubdirectories,
+			boolean listDirecotries) {
+		if (!dir.exists()) return;
+		if (!dir.isDirectory()) return;
+		for (File file : dir.listFiles()) {
+			if (file.isDirectory()) {
+				if (listDirecotries) ret.add(prefix == null ? file.getName() : prefix + file.getName());
+				if (includeSubdirectories) {
+					String filename = file.getName();
+					if (prefix != null) filename = prefix + filename;
+					addFilenames(ret, file, filename + "/", includeSubdirectories, listDirecotries);
+				}
+			} else {
+				ret.add(prefix == null ? file.getName() : prefix + file.getName());
+			}
+		}
 	}
 
 	public final File getFile(String relativePath) {
