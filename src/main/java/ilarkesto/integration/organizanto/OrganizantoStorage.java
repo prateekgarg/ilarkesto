2fef84aeabe1c998828c35ba5ce7535e6f4ab4ed
Witoslaw Koczewski <wi@koczewski.de>
2015-04-13 10:26:20 +0200

    organizanto storage

--- src/main/java/ilarkesto/integration/organizanto/OrganizantoStorage.java
+++ src/main/java/ilarkesto/integration/organizanto/OrganizantoStorage.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- *
+ * 
  * You should have received a copy of the GNU Affero General Public License along with this program. If not,
  * see <http://www.gnu.org/licenses/>.
  */
@@ -63,7 +63,7 @@ public class OrganizantoStorage {
 	}
 
 	private MapBuilder<String, String> createParams() {
-		MapBuilder<String, String> params = new MapBuilder<String, String>();
+		MapBuilder<String, String> params = new MapBuilder<String, String>(true);
 		params.put("volume", volume);
 		params.put("accessKey", accessKey);
 		return params;
