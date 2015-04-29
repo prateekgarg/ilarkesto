1f8320b06c63e0c4ee42102cca07030716046959
Witoslaw Koczewski <wi@koczewski.de>
2015-04-29 08:47:57 +0200

    min,max in sum builder

--- src/main/java/ilarkesto/core/base/BigDecimalSumBuilder.java
+++ src/main/java/ilarkesto/core/base/BigDecimalSumBuilder.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- *
+ * 
  * You should have received a copy of the GNU Affero General Public License along with this program. If not,
  * see <http://www.gnu.org/licenses/>.
  */
@@ -22,17 +22,17 @@ import java.math.BigDecimal;
 public class BigDecimalSumBuilder implements Formatable {
 
 	private BigDecimal sum;
+	private BigDecimal min;
+	private BigDecimal max;
 	private int count;
 
 	public BigDecimalSumBuilder add(Number value) {
 		count++;
 		if (value == null) return this;
 		BigDecimal bdValue = ((value instanceof BigDecimal) ? (BigDecimal) value : new BigDecimal(value.toString()));
-		if (sum == null) {
-			sum = bdValue;
-			return this;
-		}
-		sum = sum.add(bdValue);
+		sum = sum == null ? bdValue : sum.add(bdValue);
+		if (min == null || bdValue.compareTo(min) < 0) min = bdValue;
+		if (max == null || bdValue.compareTo(max) > 0) max = bdValue;
 		return this;
 	}
 
@@ -42,6 +42,14 @@ public class BigDecimalSumBuilder implements Formatable {
 
 	public BigDecimal getAvg(int scale) {
 		return sum.divide(new BigDecimal(count), scale, BigDecimal.ROUND_HALF_UP);
+	}
+
+	public BigDecimal getMin() {
+		return min;
+	}
+
+	public BigDecimal getMax() {
+		return max;
 	}
 
 	@Override
