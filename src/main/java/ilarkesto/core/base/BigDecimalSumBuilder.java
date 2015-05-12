8c7781329a1c8d61ca9b0e6d7d2d3d9bd7592af4
Witoslaw Koczewski <wi@koczewski.de>
2015-05-12 11:44:20 +0200

    sum builder

--- src/main/java/ilarkesto/core/base/BigDecimalSumBuilder.java
+++ src/main/java/ilarkesto/core/base/BigDecimalSumBuilder.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License along with this program. If not,
  * see <http://www.gnu.org/licenses/>.
  */
@@ -41,6 +41,7 @@ public class BigDecimalSumBuilder implements Formatable {
 	}
 
 	public BigDecimal getAvg(int scale) {
+		if (sum == null) return null;
 		if (count == 0) return null;
 		return sum.divide(new BigDecimal(count), scale, BigDecimal.ROUND_HALF_UP);
 	}
