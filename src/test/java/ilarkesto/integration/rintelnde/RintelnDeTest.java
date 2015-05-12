65833f1b86eb785f280b9acec4168ffebafe0426
Witoslaw Koczewski <wi@koczewski.de>
2015-05-12 11:58:31 +0200

    unit test rinteln.de

--- src/test/java/ilarkesto/integration/rintelnde/RintelnDeTest.java
+++ src/test/java/ilarkesto/integration/rintelnde/RintelnDeTest.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- *
+ * 
  * You should have received a copy of the GNU Affero General Public License along with this program. If not,
  * see <http://www.gnu.org/licenses/>.
  */
@@ -76,8 +76,8 @@ public class RintelnDeTest extends ATest {
 	@Test
 	public void downloadBranchenbuchEntriesVerbaende() throws ParseException {
 		List<Entry> verbaende = RintelnDe.downloadBranchenbuchEntries(28, observer);
-		assertSize(verbaende, 11);
-		Entry volkshochschule = verbaende.get(10);
+		assertSize(verbaende, 10);
+		Entry volkshochschule = verbaende.get(9);
 		assertEquals(volkshochschule.getLabel(), "Ver.di Senioren Ortsverein Rinteln");
 	}
 
