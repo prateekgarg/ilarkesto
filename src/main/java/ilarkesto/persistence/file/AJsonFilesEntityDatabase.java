70787d9ce9640cd997af73796732aec4c103071b
Witoslaw Koczewski <wi@koczewski.de>
2015-05-13 08:28:13 +0200

    json database

--- src/main/java/ilarkesto/persistence/file/AJsonFilesEntityDatabase.java
+++ src/main/java/ilarkesto/persistence/file/AJsonFilesEntityDatabase.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License along with this program. If not,
  * see <http://www.gnu.org/licenses/>.
  */
@@ -222,7 +222,7 @@ public abstract class AJsonFilesEntityDatabase extends ACachingEntityDatabase {
 			sb.append("* ").append(entry.getKey().getSimpleName()).append(": ").append(entry.getValue()).append("\n");
 		}
 
-		if (git.isInitialized()) sb.append("\nGit status: ").append(git.status());
+		if (git.isInitialized()) sb.append("\nGit status: ").append(git.status()).append("\n");
 
 		return sb.toString();
 	}
