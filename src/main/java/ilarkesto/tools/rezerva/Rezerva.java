0c79f21196e57fc9bca25847dae671a48b79367b
Witoslaw Koczewski <wi@koczewski.de>
2015-05-20 23:13:59 +0200

    rezerva

--- src/main/java/ilarkesto/tools/rezerva/Rezerva.java
+++ src/main/java/ilarkesto/tools/rezerva/Rezerva.java
@@ -1,20 +1,21 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License along with this program. If not,
  * see <http://www.gnu.org/licenses/>.
  */
 package ilarkesto.tools.rezerva;
 
 import ilarkesto.io.IO;
+import ilarkesto.protocol.ProtocolWriter;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -23,10 +24,30 @@ import java.util.List;
 public class Rezerva {
 
 	private File dir;
+	private ProtocolWriter prot;
 
 	public Rezerva(File dir) {
 		super();
 		this.dir = dir;
+	}
+
+	public ProtocolWriter process() {
+		prot = new ProtocolWriter();
+		for (BackupConfig backupConfig : loadBackupConfigs()) {
+			prot.pushContext(backupConfig.toString());
+			try {
+				process(backupConfig);
+			} catch (Exception ex) {
+				prot.error(ex);
+			} finally {
+				prot.popContext();
+			}
+		}
+		return prot;
+	}
+
+	private void process(BackupConfig backupConfig) {
+
 	}
 
 	private List<BackupConfig> loadBackupConfigs() {
@@ -37,6 +58,10 @@ public class Rezerva {
 			ret.add(new BackupConfig(this, file));
 		}
 		return ret;
+	}
+
+	public ProtocolWriter getProt() {
+		return prot;
 	}
 
 }
