b3e2ed91bc86de617f1466a09dc02575df701646
Witoslaw Koczewski <wi@koczewski.de>
2015-05-21 16:56:29 +0200

    enhavo

--- src/main/java/ilarkesto/tools/enhavo/FilePageContext.java
+++ src/main/java/ilarkesto/tools/enhavo/FilePageContext.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- *
+ * 
  * You should have received a copy of the GNU Affero General Public License along with this program. If not,
  * see <http://www.gnu.org/licenses/>.
  */
@@ -83,8 +83,10 @@ public class FilePageContext extends APageContext implements TemplateResolver {
 	private Context createTemplateContext() {
 		Context context = new Context();
 		context.setTemplateResolver(this);
-		context.setScope(content);
+
 		context.put("content", content);
+		context.setScope(content);
+
 		return context;
 	}
 
