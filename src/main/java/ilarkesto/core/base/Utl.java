c87f74598d251fc08aa48ad2cffde19597dd0d75
Witoslaw Koczewski <wi@koczewski.de>
2015-06-04 21:04:49 +0200

    SumBuilder

--- src/main/java/ilarkesto/core/base/Utl.java
+++ src/main/java/ilarkesto/core/base/Utl.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
@@ -33,6 +33,11 @@ import java.util.Set;
 public class Utl {
 
 	public static final BigDecimal BD_HUNDRED = new BigDecimal(100);
+
+	public static BigDecimal multiply(BigDecimal fak1, BigDecimal fak2) {
+		if (fak1 == null || fak2 == null) return null;
+		return fak1.multiply(fak2);
+	}
 
 	public static BigDecimal divide(BigDecimal dividend, BigDecimal divisor) {
 		if (dividend == null) return null;
