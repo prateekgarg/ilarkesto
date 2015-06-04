c0ed1e604cf3005bc5a28169071608fe017b2e91
Witoslaw Koczewski <wi@koczewski.de>
2015-06-04 15:02:24 +0200

    test.de

--- src/test/java/ilarkesto/integration/testde/TestDeTest.java
+++ src/test/java/ilarkesto/integration/testde/TestDeTest.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License along with this program. If not,
  * see <http://www.gnu.org/licenses/>.
  */
@@ -197,45 +197,6 @@ public class TestDeTest extends ATest {
 			assertTrue(file.exists());
 			assertTrue(file.length() > 23000);
 			log.info(file.getAbsolutePath());
-		} finally {
-			TestDe.logout(observer);
-		}
-	}
-
-	@Test
-	public void staubsauger() throws ParseException {
-		ArticleRef ref = new ArticleRef(new Date(2014, 02, 7), "Staubsauger: 74 Boden­staub­sauger im Test",
-				"Staubsauger-im-Test-1838262-0");
-		Article article = TestDe.downloadArticle(ref, observer);
-		log.info(article);
-		List<SubArticleRef> subArticles = article.getSubArticles();
-		assertNotEmpty(subArticles);
-		for (SubArticleRef sub : subArticles) {
-			log.info("  ", sub);
-		}
-
-		String summary = article.getSummary();
-		assertContains(summary, "Ob Beutelsauger oder Sauger mit Staubbox");
-		assertContains(summary, "Einem Gerät reichen bereits 870 Watt für gute Saugergebnisse.");
-
-		assertSize(subArticles, 10);
-
-		SubArticleRef subArticleStaubsauger = subArticles.get(8);
-		assertEquals(subArticleStaubsauger.getTitle(), "Staubsauger");
-		assertTrue(subArticleStaubsauger.isLocked());
-
-		SubArticleRef subArticlePdf = subArticles.get(9);
-		assertEquals(subArticlePdf.getTitle(), "Heft­artikel als PDF");
-		assertFalse(subArticlePdf.isPdf());
-
-		TestDe.login(loginData, observer);
-		try {
-			article = TestDe.downloadArticle(ref, observer);
-			subArticles = article.getSubArticles();
-
-			subArticleStaubsauger = subArticles.get(8);
-			assertFalse(subArticleStaubsauger.isLocked());
-			assertEquals(subArticleStaubsauger.getPageRef(), "Staubsauger-im-Test-1838262-2838262");
 		} finally {
 			TestDe.logout(observer);
 		}
