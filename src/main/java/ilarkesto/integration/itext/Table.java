5f737e11988226a266cb0906b74114892fd3a0d7
Witoslaw Koczewski <wi@koczewski.de>
2015-06-24 22:11:23 +0200

    iText debugging

--- src/main/java/ilarkesto/integration/itext/Table.java
+++ src/main/java/ilarkesto/integration/itext/Table.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
@@ -92,9 +92,10 @@ public class Table extends ATable implements ItextElement {
 	@Override
 	public ARow row() {
 		if (!rows.isEmpty()) {
-			ARow lastRow = rows.get(rows.size() - 1);
+			int lastRowIndex = rows.size() - 1;
+			ARow lastRow = rows.get(lastRowIndex);
 			if (lastRow.getCellCount() != getColumnCount())
-				throw new IllegalStateException("Previous row has " + lastRow.getCellCount()
+				throw new IllegalStateException("Previous row (" + lastRowIndex + ") has " + lastRow.getCellCount()
 						+ " cells, while table specifies " + getColumnCount() + " columns.");
 		}
 
