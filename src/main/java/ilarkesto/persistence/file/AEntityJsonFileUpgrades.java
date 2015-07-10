b3ccda5b10993917a2cb6a61f4d0a44f685eecda
Witoslaw Koczewski <wi@koczewski.de>
2015-07-10 08:20:57 +0200

    persistence

--- src/main/java/ilarkesto/persistence/file/AEntityJsonFileUpgrades.java
+++ src/main/java/ilarkesto/persistence/file/AEntityJsonFileUpgrades.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License along with this program. If not,
  * see <http://www.gnu.org/licenses/>.
  */
@@ -123,8 +123,9 @@ public abstract class AEntityJsonFileUpgrades {
 
 	public void upgradeEntity(File file, Class<? extends AEntity> entityType, int fileVersion) {
 		for (FileUpgrader upgrader : upgraders) {
-			if (fileVersion < upgrader.getChangeInsertionVersion() && entityType.equals(upgrader.getEntityType()))
-				upgrader.upgrade(file, fileVersion);
+			Class upgraderType = upgrader.getEntityType();
+			if (fileVersion < upgrader.getChangeInsertionVersion()
+					&& (upgraderType == null || entityType.equals(upgraderType))) upgrader.upgrade(file, fileVersion);
 
 		}
 	}
