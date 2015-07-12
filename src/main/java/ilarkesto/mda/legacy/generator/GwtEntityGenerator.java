d3d70112257dc08a261832b8dad28f3c062cf3cc
Witoslaw Koczewski <wi@koczewski.de>
2015-07-12 15:15:56 +0200

    refactoring gwt mda

--- src/main/java/ilarkesto/mda/legacy/generator/GwtEntityGenerator.java
+++ src/main/java/ilarkesto/mda/legacy/generator/GwtEntityGenerator.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
@@ -285,7 +285,7 @@ public class GwtEntityGenerator extends ABeanGenerator<EntityModel> {
 
 	private void type() {
 		ln();
-		ln("    public static final String ENTITY_TYPE = \"" + Str.lowercaseFirstLetter(bean.getName()) + "\";");
+		ln("    public static final String ENTITY_TYPE = \"" + bean.getName() + "\";");
 		ln();
 		ln("    @Override");
 		ln("    public final String getEntityType() {");
