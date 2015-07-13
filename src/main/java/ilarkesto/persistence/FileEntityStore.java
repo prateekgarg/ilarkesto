c1a6dbdac32c7fa5dd78a86e34c630996944d481
Witoslaw Koczewski <wi@koczewski.de>
2015-07-13 12:34:44 +0200

    refactoring persistence

--- src/main/java/ilarkesto/persistence/FileEntityStore.java
+++ src/main/java/ilarkesto/persistence/FileEntityStore.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
@@ -463,7 +463,8 @@ public class FileEntityStore implements EntityStore {
 
 		@Override
 		protected void prepare() {
-			file = new File(dir + "/" + entity.getDao().getEntityName() + "/" + entity.getId() + ".xml");
+			file = new File(dir + "/" + Str.lowercaseFirstLetter(entity.getDao().getEntityName()) + "/"
+					+ entity.getId() + ".xml");
 
 			// backup
 			if (file.exists() && !(entity instanceof BackupHostile)) {
