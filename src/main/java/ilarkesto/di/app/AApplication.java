0de359c21c6608271ba2078bfcfb9f35d9147daa
Witoslaw Koczewski <wi@koczewski.de>
2015-07-23 20:31:27 +0200

    application initialization

--- src/main/java/ilarkesto/di/app/AApplication.java
+++ src/main/java/ilarkesto/di/app/AApplication.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
@@ -96,12 +96,21 @@ public abstract class AApplication {
 			Persistence.initialize(backend, tm);
 
 			if (Persistence.backend != null) {
-				DaoService ds = getDaoService();
-				if (ds != null) {
-					ds.ensureIntegrity();
-				} else {
-					EntityIntegrityEnsurer.runForAll();
-				}
+
+				Persistence.runInTransaction("persistence-init", new Runnable() {
+
+					@Override
+					public void run() {
+						DaoService ds = getDaoService();
+						if (ds != null) {
+							ds.ensureIntegrity();
+						} else {
+							EntityIntegrityEnsurer.runForAll();
+						}
+					}
+
+				});
+
 			}
 
 		} else {
