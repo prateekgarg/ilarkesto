e7f60ff823c4bb4022c602a664ddb081bcb2c2fb
Witoslaw Koczewski <wi@koczewski.de>
2015-07-23 19:43:40 +0200

    application initialization

--- src/main/java/ilarkesto/di/app/AApplication.java
+++ src/main/java/ilarkesto/di/app/AApplication.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
@@ -94,21 +94,22 @@ public abstract class AApplication {
 						: new ThreadlocalTransactionManager();
 			}
 			Persistence.initialize(backend, tm);
+
+			if (Persistence.backend != null) {
+				DaoService ds = getDaoService();
+				if (ds != null) {
+					ds.ensureIntegrity();
+				} else {
+					EntityIntegrityEnsurer.runForAll();
+				}
+			}
+
 		} else {
 			log.debug("No persistence backend");
 		}
 	}
 
-	protected void ensureIntegrity() {
-		if (Persistence.backend != null) {
-			DaoService ds = getDaoService();
-			if (ds != null) {
-				ds.ensureIntegrity();
-			} else {
-				EntityIntegrityEnsurer.runForAll();
-			}
-		}
-	}
+	protected void ensureIntegrity() {}
 
 	protected boolean isSingleton() {
 		return true;
