2916f9ca4f1d0b1db1a841ef37342bbbd53e05f6
Witoslaw Koczewski <wi@koczewski.de>
2015-07-23 13:10:04 +0200

    refactoring persistence

--- src/main/java/ilarkesto/mda/legacy/generator/DatobGenerator.java
+++ src/main/java/ilarkesto/mda/legacy/generator/DatobGenerator.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
@@ -35,6 +35,7 @@ import ilarkesto.mda.legacy.model.PropertyModel;
 import ilarkesto.mda.legacy.model.ReferenceListPropertyModel;
 import ilarkesto.mda.legacy.model.ReferencePropertyModel;
 import ilarkesto.mda.legacy.model.ReferenceSetPropertyModel;
+import ilarkesto.mda.legacy.model.SetPropertyModel;
 import ilarkesto.mda.legacy.model.StringPropertyModel;
 import ilarkesto.persistence.ADatob;
 import ilarkesto.persistence.ADatobManager;
@@ -502,9 +503,9 @@ public class DatobGenerator<D extends DatobModel> extends ABeanGenerator<D> {
 			}
 		} else {
 			if (p.isCollection()) {
+				SetPropertyModel sP = (SetPropertyModel) p;
 				if (p.isValueObject()) {
-					ln("        return cloneValueObjects(" + p.getName() + ", get"
-							+ Str.uppercaseFirstLetter(p.getName()) + "Manager());");
+					ln("        return new " + sP.getCollectionImpl() + "(" + p.getName() + ");");
 				} else {
 					ln("        return new " + getFieldImpl(p) + "(" + p.getName() + ");");
 				}
