40ef1cefb427cbf906d522d2929a20363a7e261f
Witoslaw Koczewski <wi@koczewski.de>
2015-07-28 07:21:54 +0200

    enhavo build protocol

--- src/main/java/ilarkesto/tools/enhavo/CmsContext.java
+++ src/main/java/ilarkesto/tools/enhavo/CmsContext.java
@@ -1,19 +1,20 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License along with this program. If not,
  * see <http://www.gnu.org/licenses/>.
  */
 package ilarkesto.tools.enhavo;
 
+import ilarkesto.core.base.RuntimeTracker;
 import ilarkesto.core.logging.Log;
 import ilarkesto.core.time.DateAndTime;
 import ilarkesto.io.IO;
@@ -72,10 +73,12 @@ public class CmsContext {
 		prot = new ProtocolWriter(new SysoutProtocolConsumer(), new HtmlProtocolConsumer(htmlProtocolBuilder));
 		prot.pushContext(dir.getAbsolutePath());
 		prot.info(DateAndTime.now().format());
+		RuntimeTracker rt = new RuntimeTracker();
 		try {
 			buildSites();
 		} finally {
 			prot.popContext();
+			prot.info(rt.getRuntimeFormated());
 			htmlProtocolBuilder.close();
 		}
 	}
