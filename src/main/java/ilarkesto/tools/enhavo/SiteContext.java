1d4c676ebb5fa1d89d816c3c5fb68b94c6b71d91
Witoslaw Koczewski <wi@koczewski.de>
2015-07-30 14:23:11 +0200

    enhavo

--- src/main/java/ilarkesto/tools/enhavo/SiteContext.java
+++ src/main/java/ilarkesto/tools/enhavo/SiteContext.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License along with this program. If not,
  * see <http://www.gnu.org/licenses/>.
  */
@@ -89,12 +89,10 @@ public class SiteContext extends ABuilder implements TemplateResolver {
 
 	private void runScript(File file) {
 		if (file.isDirectory()) return;
-		File dst = new File(outputDir + "/" + file.getName());
-		IO.copyFile(file, dst);
 		String executorCommand = "bash";
 
 		Proc proc = new Proc(executorCommand);
-		proc.addParameter(dst.getAbsolutePath());
+		proc.addParameter(file.getAbsolutePath());
 		proc.setWorkingDir(outputDir);
 		proc.addEnvironmentParameter("CMS_INPUT_DIR", cms.getInputDir().getAbsolutePath());
 		proc.addEnvironmentParameter("CMS_OUTPUT_DIR", cms.getOutputDir().getAbsolutePath());
