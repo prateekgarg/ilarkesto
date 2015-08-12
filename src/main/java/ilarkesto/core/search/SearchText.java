b289d9aa17761a16e6c9e001b7f177d3b966ca86
Witoslaw Koczewski <wi@koczewski.de>
2015-08-12 16:24:50 +0200

    search api

--- src/main/java/ilarkesto/core/search/SearchText.java
+++ src/main/java/ilarkesto/core/search/SearchText.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License along with this program. If not,
  * see <http://www.gnu.org/licenses/>.
  */
@@ -18,11 +18,10 @@ import ilarkesto.core.base.Str;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
 public class SearchText {
 
-	private List<String> words;
+	private Collection<String> words;
 
 	public SearchText(String text) {
 		words = new ArrayList<String>();
@@ -34,6 +33,15 @@ public class SearchText {
 			idx = text.indexOf(' ');
 		}
 		words.add(text.trim());
+	}
+
+	public SearchText(Collection<String> words) {
+		super();
+		this.words = words;
+	}
+
+	public Collection<String> getWords() {
+		return words;
 	}
 
 	public <C extends Collection<Searchable>> C collectMatching(C resultContainer,
