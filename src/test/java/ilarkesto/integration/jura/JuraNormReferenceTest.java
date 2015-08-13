3de24c8be8431a87334c65079e3b5524330039b1
Witoslaw Koczewski <wi@koczewski.de>
2015-08-13 15:02:29 +0200

    jura references

--- src/test/java/ilarkesto/integration/jura/JuraNormReferenceTest.java
+++ src/test/java/ilarkesto/integration/jura/JuraNormReferenceTest.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License along with this program. If not,
  * see <http://www.gnu.org/licenses/>.
  */
@@ -19,14 +19,17 @@ import ilarkesto.testng.ATest;
 
 import org.testng.annotations.Test;
 
-public class NormReferenceTest extends ATest {
+public class JuraNormReferenceTest extends ATest {
 
 	@Test
 	public void parse() throws ParseException {
 		assertNorm(JuraNormReference.parse("§ 102 BGB"), "BGB", "102", null);
 		assertNorm(JuraNormReference.parse("§ 102 I, II BGB"), "BGB", "102", "I, II");
+
 		assertNorm(JuraNormReference.parse("§§ 102, 103 BGB"), "BGB", "102", "103");
 		assertNorm(JuraNormReference.parse("§§ 102-107 BGB"), "BGB", "102", "-107");
+
+		assertNorm(JuraNormReference.parse("Art. 5 GG"), "GG", "5", null);
 	}
 
 	@Test
