8533dae36dee5eee0c5afdeff4b6e2ebe7563b0a
Witoslaw Koczewski <wi@koczewski.de>
2015-09-12 15:26:20 +0200

    gwt desktop

--- src/main/java/ilarkesto/gwt/client/desktop/Desktop.java
+++ src/main/java/ilarkesto/gwt/client/desktop/Desktop.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License along with this program. If not,
  * see <http://www.gnu.org/licenses/>.
  */
@@ -38,9 +38,10 @@ public class Desktop {
 
 	public static void initialize() {
 		log.info("initialize()");
-		RootPanel.get("logoContainer").add(new SidebarToggleWidget());
 
 		sidebar = Widgets.desktopSidebar();
+		if (sidebar != null) RootPanel.get("logoContainer").add(new SidebarToggleWidget());
+
 		RootPanel.get("sidebarContainer").add(sidebar);
 
 		log.info("Initialized.");
@@ -63,7 +64,7 @@ public class Desktop {
 	}
 
 	public static void showWorkspace(Workspace workspace) {
-		sidebar.hide();
+		if (sidebar != null) sidebar.hide();
 
 		RootPanel workspaceContainer = RootPanel.get("workspaceContainer");
 		workspaceContainer.clear();
@@ -97,7 +98,7 @@ public class Desktop {
 				@Override
 				public void onClick(ClickEvent event) {
 					focusPanel.setFocus(false);
-					Desktop.sidebar.toggle();
+					if (sidebar != null) sidebar.toggle();
 				}
 			});
 		}
