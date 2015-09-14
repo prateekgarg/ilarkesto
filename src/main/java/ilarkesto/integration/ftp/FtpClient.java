a81aba5ad05768fb1d4594798f27a65f7f3e488d
Witoslaw Koczewski <wi@koczewski.de>
2015-09-14 20:50:04 +0200

    ftp

--- src/main/java/ilarkesto/integration/ftp/FtpClient.java
+++ src/main/java/ilarkesto/integration/ftp/FtpClient.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License along with this program. If not,
  * see <http://www.gnu.org/licenses/>.
  */
@@ -17,6 +17,7 @@ package ilarkesto.integration.ftp;
 import ilarkesto.core.auth.LoginData;
 import ilarkesto.core.auth.LoginDataProvider;
 import ilarkesto.core.base.Filepath;
+import ilarkesto.core.base.Str;
 import ilarkesto.core.base.Utl;
 import ilarkesto.core.logging.Log;
 import ilarkesto.io.IO;
@@ -122,9 +123,9 @@ public class FtpClient {
 	}
 
 	public void uploadFiles(String path, File[] files) {
-		createDir(path);
+		if (!Str.isBlank(path)) createDir(path);
 		for (File file : files) {
-			String filePath = path + "/" + file.getName();
+			String filePath = Str.isBlank(path) ? file.getName() : path + "/" + file.getName();
 			if (file.isDirectory()) {
 				createDir(filePath);
 				uploadFiles(filePath, file.listFiles());
