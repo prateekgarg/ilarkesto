3f019cce29182bf84fbc8db5e348d0b8b2b0ff2b
Witoslaw Koczewski <wi@koczewski.de>
2015-09-20 12:19:37 +0200

    gwt action toolti

--- src/main/java/ilarkesto/gwt/client/desktop/ActionButton.java
+++ src/main/java/ilarkesto/gwt/client/desktop/ActionButton.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- *
+ * 
  * You should have received a copy of the GNU Affero General Public License along with this program. If not,
  * see <http://www.gnu.org/licenses/>.
  */
@@ -55,7 +55,10 @@ public class ActionButton implements IsWidget {
 				} else {
 					button.getElement().appendChild(icon.getElement());
 					button.addStyleDependentName("iconOnly");
-					button.setTitle(action.getLabel());
+					String title = action.getLabel();
+					String tooltip = action.getTooltip();
+					if (!Str.isBlank(tooltip)) title += ": " + tooltip;
+					button.setTitle(title);
 				}
 			} else {
 				button.addStyleDependentName("disabled");
