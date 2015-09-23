8206f8e8ac0830d2c709820dd3c832f03a66dd55
Witoslaw Koczewski <wi@koczewski.de>
2015-09-23 14:23:54 +0200

    ftp

--- src/main/java/ilarkesto/integration/ftp/FtpClient.java
+++ src/main/java/ilarkesto/integration/ftp/FtpClient.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License along with this program. If not,
  * see <http://www.gnu.org/licenses/>.
  */
@@ -55,11 +55,13 @@ public class FtpClient {
 	}
 
 	public void delete(String path) {
+		boolean deleted;
 		try {
-			client.deleteFile(path);
+			deleted = client.deleteFile(path);
 		} catch (IOException ex) {
 			throw new RuntimeException(ex);
 		}
+		if (!deleted) throw new RuntimeException("Deleting remote file failed: " + path);
 	}
 
 	public List<FTPFile> listFiles(String path) {
