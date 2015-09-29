6de0a161693fda59105ebb9d3b1664572c6a4b5b
Witoslaw Koczewski <wi@koczewski.de>
2015-09-29 11:49:02 +0200

    gwt

--- src/main/java/ilarkesto/gwt/client/desktop/fields/AEditableField.java
+++ src/main/java/ilarkesto/gwt/client/desktop/fields/AEditableField.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- *
+ * 
  * You should have received a copy of the GNU Affero General Public License along with this program. If not,
  * see <http://www.gnu.org/licenses/>.
  */
@@ -57,7 +57,7 @@ public abstract class AEditableField extends AField {
 
 	@Override
 	public Widget createLabelWidget(String text) {
-		if (isMandatory()) {
+		if (isMandatory() && !isValueSet()) {
 			Widget label = super.createLabelWidget(text);
 			Label marker = Widgets.textWarning("*");
 			marker.getElement().getStyle().setProperty("fontSize", label.getElement().getStyle().getFontSize());
