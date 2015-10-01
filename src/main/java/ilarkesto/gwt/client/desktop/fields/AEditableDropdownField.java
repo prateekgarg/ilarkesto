5872ae3f450a31bce635bb52cbda872cb0edd037
Kacper Grubalski <kacper@grubalski.de>
2015-10-01 17:33:33 +0200

    Field uses radio buttons in horizontal layout if options <= 2 and in vertical if options <= 20 for faster selection.

--- src/main/java/ilarkesto/gwt/client/desktop/fields/AEditableDropdownField.java
+++ src/main/java/ilarkesto/gwt/client/desktop/fields/AEditableDropdownField.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License along with this program. If not,
  * see <http://www.gnu.org/licenses/>.
  */
@@ -19,23 +19,34 @@ import ilarkesto.core.base.Utl;
 import ilarkesto.gwt.client.desktop.Widgets;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.google.gwt.dom.client.Style;
+import com.google.gwt.dom.client.Style.Display;
 import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.event.dom.client.ChangeEvent;
 import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.RadioButton;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 public abstract class AEditableDropdownField extends AEditableField {
 
 	private static final String NULL_KEY = "__NULL__";
 
 	private ListBox listBox;
+
+	private Map<String, RadioButton> radioButtons;
 
 	public abstract void applyValue(String value);
 
@@ -50,15 +61,101 @@ public abstract class AEditableDropdownField extends AEditableField {
 
 	@Override
 	public void trySubmit() throws RuntimeException {
-		int selectedIndex = listBox.getSelectedIndex();
-		String value = selectedIndex < 0 ? null : listBox.getValue(selectedIndex);
-		if (NULL_KEY.equals(value)) value = null;
+		String value = getSelectedValue();
 		if (value == null && isMandatory()) throw new RuntimeException("Auswahl erforderlich");
 		applyValue(value);
 	}
 
+	private String getSelectedValue() {
+		if (isShowAsRadioButtons(getOptionKeys())) {
+			for (String value : radioButtons.keySet()) {
+				RadioButton radioButton = radioButtons.get(value);
+				if (!radioButton.getValue()) continue;
+				return value;
+			}
+		} else {
+			int selectedIndex = listBox.getSelectedIndex();
+			String value = selectedIndex < 0 ? null : listBox.getValue(selectedIndex);
+			if (NULL_KEY.equals(value)) value = null;
+			return value;
+		}
+		return null;
+	}
+
 	@Override
 	public IsWidget createEditorWidget() {
+		// return createListBox();
+		return isShowAsRadioButtons(getOptionKeys()) ? createRadioButtonPanel() : createListBox();
+	}
+
+	private Collection<String> getOptionKeys() {
+		return getOptions().getKeys();
+	}
+
+	protected boolean isShowAsRadioButtons(Collection<String> optionKeys) {
+		return isShowAsRadioButtons() && optionKeys.size() <= 20;
+	}
+
+	protected boolean isShowAsRadioButtons() {
+		if (getParent() == null) return true;
+		if (isRadioButtonsHorizontal(getOptionKeys())) return true;
+		return !isParentMultiField();
+	}
+
+	private boolean isParentMultiField() {
+		return getParent() instanceof AEditableMultiFieldField;
+	}
+
+	protected boolean isRadioButtonsHorizontal(Collection<String> optionKeys) {
+		return optionKeys.size() <= 2;
+	}
+
+	private Panel createRadioButtonPanel() {
+		radioButtons = new HashMap<String, RadioButton>();
+
+		boolean horizontal = isRadioButtonsHorizontal(getOptionKeys());
+		Panel panel = horizontal ? new FlowPanel() : new VerticalPanel();
+		for (String key : getOptionKeys()) {
+			String label = getOptions().getValueForKey(key);
+
+			RadioButton radioButton = new RadioButton(getId(), label);
+			radioButton.getElement().setId(getId() + "_radiobutton_");
+
+			final boolean selected = key.equals(getSelectedOptionKey());
+			radioButton.setValue(selected);
+
+			if (getEditVetoMessage() == null) {} else {
+				radioButton.setEnabled(false);
+				radioButton.setTitle(getEditVetoMessage());
+			}
+			Style style = radioButton.getElement().getStyle();
+			style.setProperty("minWidth", "100px");
+			style.setProperty("minHeight", "32px");
+			style.setDisplay(Display.BLOCK);
+			style.setFloat(com.google.gwt.dom.client.Style.Float.LEFT);
+			style.setWidth(horizontal ? getTextBoxWidth() / 2 : getTextBoxWidth(), Unit.PX);
+			style.setMarginRight(Widgets.defaultSpacing, Unit.PX);
+
+			if (!isParentMultiField()) {
+				radioButton.addValueChangeHandler(new ValueChangeHandler<Boolean>() {
+
+					@Override
+					public void onValueChange(ValueChangeEvent<Boolean> event) {
+						if (fieldEditorDialogBox == null) return;
+						fieldEditorDialogBox.submit();
+					}
+				});
+			}
+
+			radioButtons.put(key, radioButton);
+			panel.add(radioButton);
+		}
+		panel.add(Widgets.clear());
+
+		return panel;
+	}
+
+	private ListBox createListBox() {
 		listBox = new ListBox();
 		listBox.getElement().setId(getId() + "_listBox");
 		Style style = listBox.getElement().getStyle();
