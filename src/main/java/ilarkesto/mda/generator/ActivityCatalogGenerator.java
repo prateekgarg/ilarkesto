3b8e465d244f65c3c9ac44bf1ad4aac0626585ae
Witoslaw Koczewski <wi@koczewski.de>
2015-10-06 23:23:40 +0200

    gwt mda

---
+++ src/main/java/ilarkesto/mda/generator/ActivityCatalogGenerator.java
@@ -0,0 +1,138 @@
+/*
+ * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with this program. If not,
+ * see <http://www.gnu.org/licenses/>.
+ */
+package ilarkesto.mda.generator;
+
+import ilarkesto.base.Comparators;
+import ilarkesto.core.base.Str;
+import ilarkesto.gwt.client.desktop.AActivity;
+import ilarkesto.io.IO;
+import ilarkesto.mda.legacy.generator.AClassGenerator;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ActivityCatalogGenerator extends AClassGenerator {
+
+	private String packageName;
+	private FileFilter fileFilter;
+
+	public ActivityCatalogGenerator(String packageName) {
+		super();
+		this.packageName = packageName;
+	}
+
+	@Override
+	protected void writeContent() {
+		writeGetActivityNames();
+		writeIsActivityAvailable();
+		writeInstantiateActivity();
+	}
+
+	private void writeGetActivityNames() {
+		ln();
+		s("    public static final " + List.class.getName() + "<String>", "ACTIVITY_NAMES = " + Arrays.class.getName()
+				+ ".asList(");
+		List<File> activities = findActivities();
+		boolean first = true;
+		for (File file : activities) {
+			if (first) {
+				first = false;
+			} else {
+				s(", ");
+			}
+			String activityName = Str.removeSuffix(file.getName(), ".java");
+			s("\"" + activityName + "\"");
+		}
+		ln(");");
+	}
+
+	private void writeIsActivityAvailable() {
+		ln();
+		ln("    public static boolean isActivityAvailable(String name) {");
+		List<File> activities = findActivities();
+		for (File file : activities) {
+			String activityName = Str.removeSuffix(file.getName(), "Activity.java");
+			ln("        if (name.equals(\"" + activityName + "\")) return true;");
+		}
+		ln("        return false;");
+		ln("    }");
+	}
+
+	private void writeInstantiateActivity() {
+		ln();
+		ln("    public static", AActivity.class.getName(), "instantiateActivity(String name) {");
+		List<File> activities = findActivities();
+		for (File file : activities) {
+			String activityName = Str.removeSuffix(file.getName(), "Activity.java");
+			String packageName = Str.removePrefix(Str.removePrefix(file.getParent(), "src/"), "src\\")
+					.replace('/', '.').replace('\\', '.');
+			String activityClass = packageName + "." + activityName + "Activity";
+			ln("        if (name.equals(\"" + activityName + "\")) return new " + activityClass + "();");
+		}
+		ln("        throw new IllegalArgumentException(\"Activity does not exist: \" + name);");
+		ln("    }");
+	}
+
+	private List<File> findActivities() {
+		List<File> activities = IO.findFiles(new File("src"), new FileFilter() {
+
+			@Override
+			public boolean accept(File file) {
+				String fileName = file.getName();
+				if (file.getName().equals("AActivity.java")) return false;
+				if (file.getParentFile().getName().equals("base") && fileName.startsWith("A")) return false;
+
+				if (fileFilter != null && !fileFilter.accept(file)) return false;
+				return file.getName().endsWith("Activity.java");
+			}
+		});
+		Collections.sort(activities, Comparators.filesByNameIgnoreCase);
+		return activities;
+	}
+
+	public ActivityCatalogGenerator setFileFilter(FileFilter filter) {
+		this.fileFilter = filter;
+		return this;
+	}
+
+	@Override
+	protected String getName() {
+		return "ActivityCatalog";
+	}
+
+	@Override
+	protected String getPackage() {
+		return packageName;
+	}
+
+	@Override
+	protected boolean isInterface() {
+		return false;
+	}
+
+	@Override
+	protected boolean isAbstract() {
+		return false;
+	}
+
+	@Override
+	protected boolean isOverwrite() {
+		return true;
+	}
+
+}
