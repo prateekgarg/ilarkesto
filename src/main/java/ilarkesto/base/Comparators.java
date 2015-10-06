d47a1b5aa6554637d7270e0e4fe35a2b8ad968b2
Witoslaw Koczewski <wi@koczewski.de>
2015-10-06 23:23:28 +0200

    Comparators

---
+++ src/main/java/ilarkesto/base/Comparators.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
+ * 
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License along with this program. If not,
+ * see <http://www.gnu.org/licenses/>.
+ */
+package ilarkesto.base;
+
+import java.io.File;
+import java.util.Comparator;
+
+public class Comparators {
+
+	public static final Comparator<File> filesByNameIgnoreCase = new Comparator<File>() {
+
+		@Override
+		public int compare(File a, File b) {
+			return a.getName().toLowerCase().compareTo(b.getName().toLowerCase());
+		}
+
+	};
+
+}
