a018ab97835ff5b93f35aed119dd83444261f309
Witoslaw Koczewski <wi@koczewski.de>
2015-10-06 23:37:11 +0200

    gwt mda

--- src/main/java/ilarkesto/mda/generator/ActivityCatalogGenerator.java
+++ src/main/java/ilarkesto/mda/generator/ActivityCatalogGenerator.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- *
+ * 
  * You should have received a copy of the GNU Affero General Public License along with this program. If not,
  * see <http://www.gnu.org/licenses/>.
  */
@@ -79,8 +79,10 @@ public class ActivityCatalogGenerator extends AClassGenerator {
 		List<File> activities = findActivities();
 		for (File file : activities) {
 			String activityName = Str.removeSuffix(file.getName(), "Activity.java");
-			String packageName = Str.removePrefix(Str.removePrefix(file.getParent(), "src/"), "src\\")
-					.replace('/', '.').replace('\\', '.');
+			String packageName = file.getParent();
+			packageName = packageName.replace('/', '.').replace('\\', '.');
+			packageName = Str.removePrefix(packageName, "src.");
+			packageName = Str.removePrefix(packageName, "main.java.");
 			String activityClass = packageName + "." + activityName + "Activity";
 			ln("        if (name.equals(\"" + activityName + "\")) return new " + activityClass + "();");
 		}
